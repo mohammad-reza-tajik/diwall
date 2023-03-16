8544aaa91bdae30c3d6b3d854d6a618f911735a3
MRT <mr.tajik1999@gmail.com>
2023-03-16 15:30:28 +0330

    replace then blocks with async-await in sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,46 @@
-self.addEventListener("install",function (event) {
-    event.waitUntil(
-        caches.open("static").then((cache)=> {
-            cache.addAll([
+const STATIC_CACHE_NAME = "static-v3";
+const DYNAMIC_CACHE_NAME = "dynamic-v1";
+
+self.addEventListener("install", function (event) {
+    event.waitUntil((async () => {
+
+            const cache = await caches.open(STATIC_CACHE_NAME);
+            await cache.addAll([
                 "/assets/pictures/hero-image-desktop.png",
+                "/assets/pictures/hero-image-mobile.jpg",
                 "/",
                 "/assets/fonts/dana-fanum-bold.woff2",
                 "/assets/fonts/dana-fanum-medium.woff2",
-                "/_next/static/chunks/main-27d355ecdf567f83.js",
-                "/_next/static/chunks/pages/_app-1e9d895f12ee46a3.js",
-                "/_next/static/chunks/pages/index-b7c5352ed1263844.js"
             ])
+
+        }
+    )())
+
+
+})
+
+self.addEventListener("activate",(event)=>{
+    event.waitUntil((async () => {
+        const keys = await caches.keys();
+        keys.forEach( key => {
+            if (key !== STATIC_CACHE_NAME)
+                caches.delete(key)
         })
+
+    })()
     )
 })
 
-
-self.addEventListener("fetch",(event)=>{
-    event.respondWith(
-        caches.match(event.request).then((res)=>{
+self.addEventListener("fetch", (event) => {
+    event.respondWith((async () => {
+            const res = await caches.match(event.request)
             if (res)
                 return res
             else
                 return fetch(event.request)
-        })
+
+        })()
     )
+
+
 })
