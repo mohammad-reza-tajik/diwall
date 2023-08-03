d5c702ea195ac80b3dece22490b3089c20ba9dfc
Morta <mr.tajik1999@gmail.com>
2023-08-03 09:43:07 +0330

    add idb.ts file to create and interact with idb easier

---
+++ utilities/idb.ts
@@ -0,0 +1,22 @@
+import localforage from "localforage";
+import {ProductType} from "../db/productModel";
+
+let objectStore : LocalForage;
+if (typeof window !== "undefined" && "indexedDB" in window) {
+    objectStore = localforage.createInstance({
+        name: "products",
+        driver: localforage.INDEXEDDB,
+        size: 8000000,
+    });
+}
+
+export const getFromIDB = async (key:string) : Promise<{ product: ProductType, relatedProducts: ProductType[] } | null> => {
+  const data : {product: ProductType , relatedProducts : ProductType[]} | null = await objectStore.getItem(key);
+  return data
+}
+
+export const saveToIDB = async (key:string,value:any) => {
+  return await objectStore.setItem(key,value);
+}
+export default objectStore;
+
