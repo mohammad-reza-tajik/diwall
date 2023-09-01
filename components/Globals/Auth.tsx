86a31af80290f1cb83a7ce87bb02627f6b03ba1a
Morta <mr.tajik1999@gmail.com>
2023-09-01 22:17:32 +0330

    remove axios from the project

--- components/Globals/Auth.tsx
+++ components/Globals/Auth.tsx
@@ -1,7 +1,6 @@
 import React, {Fragment, useEffect} from "react";
 import {userActions , useAppDispatch} from "@/store"
-import axios from "axios"
-import {useRouter} from "next/router";
+import useFetch from "@/hooks/useFetch";
 
 interface Props {
     children: React.ReactNode;
@@ -9,23 +8,17 @@ interface Props {
 
 const Auth: React.FC<Props> = ({children}) => {
 
-
     const dispatch = useAppDispatch()
-    const router = useRouter();
-
     useEffect(() => {
 
         /* useEffect always executes on client side */
 
         (async () => {
-
             try {
-
                 const token = localStorage.getItem("token")
                 const _id = localStorage.getItem("_id")
                 if (_id && _id !== "undefined") {
-                    const res = await axios.post("/api/get-user", {_id, token});
-                    // console.log(res)
+                    const res = await useFetch.post("/api/get-user", {_id, token});
                     dispatch(userActions.login({user : res.user , token : res.token}))
                 }
              } catch(err) {
@@ -33,9 +26,6 @@ const Auth: React.FC<Props> = ({children}) => {
                     dispatch(userActions.logout())
                     console.log(err)
                 }
-                
-                
-            
         })()
             
 
