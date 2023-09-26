be57349c081e9c116a07d3088e4e821a018b412f
Morta <mr.tajik1999@gmail.com>
2023-09-27 00:37:48 +0330

    reduce fontSizes in Profile component

--- components/AccountPage/Profile.tsx
+++ components/AccountPage/Profile.tsx
@@ -2,61 +2,60 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import type {User} from "@/store/userSlice";
+import {SxProps} from "@mui/system";
 
+const styles = {
+   keys : {
+       fontSize: {xs: 12, md: 16}
+   }
+} satisfies Record<string, SxProps>
 
 interface Props {
     user:User;
 }
-const Profile : React.FC<Props> = (props) => {
-    const {user} =props;
+const Profile : React.FC<Props> = ({user}) => {
 
     return (
-        <Grid container item xs={12} py={20} px={{xs: 5, md: 40}} gap={40}>
+        <Grid container item xs={12} py={20} pr={{xs: 0, md: 40}} spacing={40}>
 
             <Grid container item xs={12} alignItems={"center"} gap={10}>
-                <Grid component={"span"} sx={{fontSize: {xs: 14, md: 20}}}>نام و نام خانوادگی
+                <Grid component={"span"} sx={styles.keys}>نام و نام خانوادگی
                     : </Grid>
-                <Typography variant={"subtitle1"} fontFamily={"dana-bold"}
-                            fontSize={{xs: 14, md: 16}}>
+                <Typography variant={"subtitle1"} sx={styles.keys}>
                     مشخص نشده !
                 </Typography>
 
             </Grid>
             <Grid container item xs={12} alignItems={"center"} gap={20}>
-                <Grid component={"span"} sx={{fontSize: {xs: 14, md: 20}}}>نام کاربری : </Grid>
-                <Typography variant={"subtitle1"} fontFamily={"dana-bold"}
-                            fontSize={{xs: 14, md: 16}}>
+                <Grid component={"span"} sx={styles.keys}>نام کاربری : </Grid>
+                <Typography variant={"subtitle1"} sx={styles.keys}>
                     {user?.username}
                 </Typography>
             </Grid>
             <Grid container item xs={12} alignItems={"center"} gap={20}>
-                <Grid component={"span"} sx={{fontSize: {xs: 14, md: 20}}}>ایمیل : </Grid>
-                <Typography variant={"subtitle1"} fontFamily={"dana-bold"}
-                            fontSize={{xs: 14, md: 16}}>
+                <Grid component={"span"} sx={styles.keys}>ایمیل : </Grid>
+                <Typography variant={"subtitle1"} sx={styles.keys}>
                     {user?.email}
                 </Typography>
 
             </Grid>
             <Grid container item xs={12} alignItems={"center"} gap={10}>
-                <Grid component={"span"} sx={{fontSize: {xs: 14, md: 20}}}>شماره موبایل : </Grid>
-                <Typography variant={"subtitle1"} fontFamily={"dana-bold"}
-                            fontSize={{xs: 14, md: 16}}>
+                <Grid component={"span"} sx={styles.keys}>شماره موبایل : </Grid>
+                <Typography variant={"subtitle1"} sx={styles.keys}>
                     مشخص نشده !
                 </Typography>
 
             </Grid>
             <Grid container item xs={12} alignItems={"center"} gap={10}>
-                <Grid component={"span"} sx={{fontSize: {xs: 14, md: 20}}}> تاریخ تولد : </Grid>
-                <Typography variant={"subtitle1"} fontFamily={"dana-bold"}
-                            fontSize={{xs: 14, md: 16}}>
+                <Grid component={"span"} sx={styles.keys}> تاریخ تولد : </Grid>
+                <Typography variant={"subtitle1"} sx={styles.keys}>
                     مشخص نشده !
                 </Typography>
 
             </Grid>
             <Grid container item xs={12} alignItems={"center"} gap={10}>
-                <Grid component={"span"} sx={{fontSize: {xs: 14, md: 20}}}> شغل : </Grid>
-                <Typography variant={"subtitle1"} fontFamily={"dana-bold"}
-                            fontSize={{xs: 14, md: 16}}>
+                <Grid component={"span"} sx={styles.keys}> شغل : </Grid>
+                <Typography variant={"subtitle1"} sx={styles.keys}>
                     مشخص نشده !
                 </Typography>
 
@@ -66,10 +65,3 @@ const Profile : React.FC<Props> = (props) => {
 }
 
 export default Profile
-
-/*
-*
-*  <Button variant={"outlined"}
-                    sx={{fontSize: {xs: 12, md: 16}, position: "absolute", top: 10, left: 10}}>تغییر
-                اطلاعات</Button>
-* */
