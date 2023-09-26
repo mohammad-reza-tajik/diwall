6c05c6641920a4a6ab8ed00d141b7894f08d82d3
Morta <mr.tajik1999@gmail.com>
2023-09-27 01:39:06 +0330

    refine mainNavigation component and replace useMediaQuery hook with responsive props

--- components/Globals/MainNavigation.tsx
+++ components/Globals/MainNavigation.tsx
@@ -2,73 +2,45 @@ import Link from "next/link";
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import {useTheme} from "@mui/material/styles";
 import LocalPhoneOutlined from "@mui/icons-material/LocalPhoneOutlined";
 import type {SxProps} from "@mui/system";
 
 
 const styles = {
-
     mainNav: {
-        fontSize: 16,
-        fontFamily: "dana-medium",
-        gap: 20
-
+        fontSize: 15,
+        gap: 20,
     },
-    mainNavLink: {
-        color: "#888",
+    link: {
+        color: "#777",
         transition: "color .4s",
-
         "&:hover": {
             color: "primary.main"
         }
-
     }
 } satisfies Record<string, SxProps>
 
 const MainNavigation: React.FC = () => {
 
-
-    const theme = useTheme()
-    const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
-    const matches1277 = useMediaQuery('(max-width:1277px)');
-
-
     return (
-        <Grid container item alignItems={"center"} justifyContent={"space-between"}>
-            {
-                !matchesMD &&
-                <>
-                    <Grid container alignItems={"center"} component={"nav"} item xs={9} sx={styles.mainNav}>
-                        <Grid component={Link} href={"/"} sx={styles.mainNavLink}>صفحه نخست</Grid>
-                        <Grid component={Link} href={"/products"} sx={styles.mainNavLink}>جدیدترین ها</Grid>
-                        <Grid component={Link} href={"/products?sortBy=2"} sx={styles.mainNavLink}>پرفروش ترین
-                            ها</Grid>
-                        <Grid component={Link} href={"/products?sortBy=3"} sx={styles.mainNavLink}>محبوب ترین
-                            ها</Grid>
-                        <Grid component={Link} href={"/about"} sx={styles.mainNavLink}>درباره ما</Grid>
-                        <Grid component={Link} href={"/collaboration"} sx={styles.mainNavLink}>همکاری با ما</Grid>
+        <Grid container item alignItems={"center"} justifyContent={"space-between"} display = {{xs: "none", md: "flex"}}>
+                    <Grid container alignItems={"center"} component={"nav"} item xs sx={styles.mainNav}>
+                        <Grid component={Link} href={"/"} sx={styles.link}>صفحه نخست</Grid>
+                        <Grid component={Link} href={"/products"} sx={styles.link}>جدیدترین ها</Grid>
+                        <Grid component={Link} href={"/products?sortBy=2"} sx={styles.link}>پرفروش ترین ها</Grid>
+                        <Grid component={Link} href={"/products?sortBy=3"} sx={styles.link}>محبوب ترین ها</Grid>
+                        <Grid component={Link} href={"/about"} sx={styles.link}>درباره ما</Grid>
+                        <Grid component={Link} href={"/collaboration"} sx={styles.link}>همکاری با ما</Grid>
                     </Grid>
-                    <Grid container item direction={"row"} xs={2} justifyContent={"flex-end"}
-                          display={matches1277 && "none"}>
-                        <Grid container item direction={"column"} xs={6}>
-                                <Typography fontWeight={700} color={"#666"} fontSize={18}
-                                            variant={"caption"}>021464879</Typography>
-
-                                <Typography color={"#666"} fontSize={12} variant={"caption"}>پشتیبانی
-                                    مشتریان</Typography>
-
-
+                    <Grid container item xs={"auto"}>
+                        <Grid container item direction={"column"} xs>
+                                <Typography fontSize={18} fontFamily={"dana-bold"}>021464879</Typography>
+                                <Typography fontSize={12}>پشتیبانی مشتریان</Typography>
                         </Grid>
-                        <Grid item container justifyContent={"center"} alignItems={"center"} xs={2}>
+                        <Grid item container justifyContent={"center"} alignItems={"center"} xs>
                             <LocalPhoneOutlined sx={{fontSize: 35}} color={"primary"}/>
                         </Grid>
                     </Grid>
-                </>
-
-            }
-
         </Grid>
     )
 }
