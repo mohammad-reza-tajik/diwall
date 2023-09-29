b7389f4f5b3f6084e92480e2561a73a080de450e
Morta <mr.tajik1999@gmail.com>
2023-09-29 19:55:52 +0330

    fix image extensions in MiddleSection

--- components/HomePage/MiddleSection.tsx
+++ components/HomePage/MiddleSection.tsx
@@ -13,11 +13,11 @@ const MiddleSection : React.FC = () => {
     return(
     <Grid container component={"section"} item xs={12} my={30}>
         <Grid container item xs={12} md={6} height={300} position={"relative"} overflow={"hidden"}  sx={styles.pictures}>
-            <Image src={"/assets/pictures/3D-posters.png"} fill sizes={"500px"}  alt={"3D-posters"} className="contain" />
+            <Image src={"/assets/pictures/3D-posters.jpg"} fill sizes={"500px"}  alt={"3D-posters"} className="contain" />
         </Grid>
         <Grid container item xs={12} md={6} height={{xs:250,sm:300}} justifyContent={"flex-end"} position={"relative"} overflow={"hidden"}>
            
-                <Image src={"/assets/pictures/customize-poster.png"} fill sizes={"500px"}  alt={"customize-poster"} className="contain" /> :
+                <Image src={"/assets/pictures/customize-poster.jpg"} fill sizes={"500px"}  alt={"customize-poster"} className="contain" /> :
                 
            
 
