52a7599782fa72efe4c30f40f9842192e5a6c36d
Morta <mr.tajik1999@gmail.com>
2023-09-30 16:49:53 +0330

    disable remove from cart button when loading in CartItem

--- components/AccountPage/CartItem.tsx
+++ components/AccountPage/CartItem.tsx
@@ -2,6 +2,7 @@ import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import Link from "next/link";
 import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
 import Delete from "@mui/icons-material/Delete";
 import RemoveCircleOutline from "@mui/icons-material/RemoveCircleOutline";
@@ -14,71 +15,86 @@ import Image from "next/image";
 import type {ProductType} from "@/db/productModel";
 import useFetch from "@/hooks/useFetch";
 import {enqueueSnackbar} from "notistack";
+import CircularProgress from "@mui/material/CircularProgress";
 
 
 const CartItem: React.FC<ProductType> = ({title, slug, _id}) => {
 
-    const router = useRouter()
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
 
     const user = useAppSelector(state => state.user)
     const dispatch = useAppDispatch()
 
     const [numberInCart, setNumberInCart] = useState(1)
-    const numbersInCartChangeHandler = (event : ChangeEvent<HTMLInputElement>) => {
-        setNumberInCart(Number(event.target .value))
+    const numbersInCartChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setNumberInCart(Number(event.target.value))
 
     }
 
     const removeFromCart = async () => {
         if (user?.username) {
-            await useFetch.delete(`/api/user/cart?productId=${_id}&_id=${user._id}&token=${user.token}`)
-            dispatch(userActions.removeFromCart(_id));
-            enqueueSnackbar("از سبد خرید شما حذف شد" , {
-                variant : "info",
-            });
+            try {
+                setIsLoading(true)
+                await useFetch.delete(`/api/user/cart?productId=${_id}&_id=${user._id}&token=${user.token}`)
+                dispatch(userActions.removeFromCart(_id));
+                enqueueSnackbar("از سبد خرید شما حذف شد", {
+                    variant: "info",
+                });
+            } catch (err) {
+                enqueueSnackbar("متاسفانه عملیات با خطا مواجه شد", {
+                    variant: "error",
+                })
+            } finally {
+                setIsLoading(true)
+            }
         }
 
     }
 
 
     return (
-            <Grid container item xs={12} bgcolor={"white.main"} p={"1rem"} borderRadius={"1rem"}>
-                <Grid className={"pointer"} container item xs={12} md alignItems={"center"} gap={10}
-                      onClick={() => router.push("/products/" + slug)}>
-                    <Grid item xs={"auto"}>
-                        <Image src={`/assets/pictures/products/${slug}.jpg`} width={50} height={50} style={{borderRadius:"50%"}}
-                                alt={title}/>
-                    </Grid>
-                    <Grid item xs component={Typography} variant={"h4"} fontSize={{xs: 12, md: 16}}>
-                            {title}
-                    </Grid>
+        <Grid container bgcolor={"white.main"} p={"1rem"} borderRadius={"1rem"}>
+            <Grid container item xs={12} md alignItems={"center"} gap={10} component={Link} href={`/products/${slug}`}>
+                <Grid item xs={"auto"}>
+                    <Image src={`/assets/pictures/products/${slug}.jpg`} width={50} height={50}
+                           style={{borderRadius: "50%"}}
+                           alt={title}/>
                 </Grid>
-                <Grid container item xs={12} md={"auto"}  alignItems={"center"}
-                      justifyContent={"flex-end"}>
+                <Grid item xs component={Typography} variant={"h4"} fontSize={{xs: 12, md: 16}}>
+                    {title}
+                </Grid>
+            </Grid>
+            <Grid container item xs={12} md={"auto"} alignItems={"center"}
+                  justifyContent={"flex-end"}>
 
-                    <IconButton onClick={() => {
-                        setNumberInCart((prevState) => Number(prevState) + 1)
-                    }}>
-                        <AddCircleOutline color={"primary"} sx={{fontSize: 25}}/>
-                    </IconButton>
-                    <TextField onChange={numbersInCartChangeHandler} value={numberInCart} sx={{width: {xs: 40, md: 80}}}
-                               color={"primary"} type={"number"} size={"small"} variant={"standard"}/>
+                <IconButton onClick={() => {
+                    setNumberInCart((prevState) => Number(prevState) + 1)
+                }}>
+                    <AddCircleOutline color={"primary"} sx={{fontSize: 25}}/>
+                </IconButton>
+                <TextField onChange={numbersInCartChangeHandler} value={numberInCart} sx={{width: {xs: 40, md: 80}}}
+                           color={"primary"} type={"number"} size={"small"} variant={"standard"}/>
 
-                    {
-                        numberInCart != 1 ?
+                {
+                    numberInCart != 1 ?
                         <IconButton onClick={() => {
                             setNumberInCart((prevState) => Number(prevState) - 1)
                         }}>
                             <RemoveCircleOutline color={"primary"} sx={{fontSize: 25}}/>
                         </IconButton> :
-                        <IconButton onClick={removeFromCart}>
-                            <Delete color={"primary"} sx={{fontSize: 25}}/>
+                        <IconButton onClick={removeFromCart} disabled={isLoading}>
+                            {
+                                isLoading ?
+                                    <CircularProgress color={"primary"} size={25}/> :
+                                    <Delete color={"primary"} sx={{fontSize: 22}}/>
+                            }
                         </IconButton>
 
-                    }
+                }
 
-                </Grid>
             </Grid>
+        </Grid>
 
     )
 
