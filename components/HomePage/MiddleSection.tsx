d7d3417aefde2b138de5175c7c0a53e74dadea1b
Morta <mr.tajik1999@gmail.com>
2023-10-01 07:49:16 +0330

    solve the responsiveness issue in MiddleSection

--- components/HomePage/MiddleSection.tsx
+++ components/HomePage/MiddleSection.tsx
@@ -3,28 +3,26 @@ import Image from "next/image"
 import React from "react";
 import type {SxProps} from "@mui/system";
 
-const styles  = {
-    pictures:{
-        display:{xs:"none",md:"flex"}
+const styles = {
+    pictures: {
+        width: "100%",
+        height: "auto"
     }
 } satisfies Record<string, SxProps>
 
-const MiddleSection : React.FC = () => {
-    return(
-    <Grid container component={"section"} item xs={12} my={30}>
-        <Grid container item xs={12} md={6} height={300} position={"relative"} overflow={"hidden"}  sx={styles.pictures}>
-            <Image src={"/assets/pictures/3D-posters.jpg"} fill sizes={"500px"}  alt={"3D-posters"} className="contain" />
+const MiddleSection: React.FC = () => {
+    return (
+        <Grid container component={"section"} my={30}>
+            <Grid container item xs={12} md={6} display={{xs: "none", md: "flex"}}>
+                <Image src={"/assets/pictures/3D-posters.jpg"} width={400} height={200} alt={"3D-posters"}
+                       style={styles.pictures} className="contain"/>
+            </Grid>
+            <Grid container item xs={12} md={6}>
+                <Image src={"/assets/pictures/customize-poster.jpg"} width={400} height={200} alt={"customize-poster"}
+                       style={styles.pictures} className="contain"/>
+            </Grid>
         </Grid>
-        <Grid container item xs={12} md={6} height={{xs:250,sm:300}} justifyContent={"flex-end"} position={"relative"} overflow={"hidden"}>
-           
-                <Image src={"/assets/pictures/customize-poster.jpg"} fill sizes={"500px"}  alt={"customize-poster"} className="contain" /> :
-                
-           
-
-        </Grid>
-
-    </Grid>
-        )
+    )
 }
 
 export default MiddleSection
