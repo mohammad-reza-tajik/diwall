2e84eb8da76a4cd0848ba414fe809c352c899403
Morta <mr.tajik1999@gmail.com>
2023-12-25 10:27:32 +0330

    feat(utils): add fetcher and remove useFetch

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,49 +1,54 @@
-const handleResponse = async (response : any) => {
+const handleResponse = async (response: any) => {
     return await response.json();
 }
-const get = async (url: string) => {
+const get = async (url: string, options?: RequestInit) => {
     const requestOptions = {
-        method: 'GET'
+        method: 'GET',
+        ...options
+
     };
     const response = await fetch(url, requestOptions);
     return handleResponse(response);
 }
 
-const post = async (url: string, body : any) => {
+const post = async (url: string, body: any, options?: RequestInit) => {
     const requestOptions = {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
+        ...options
     };
     const response = await fetch(url, requestOptions);
     return handleResponse(response);
 }
 
-const put = async (url: string, body : any) => {
+const put = async (url: string, body: any, options?: RequestInit) => {
     const requestOptions = {
         method: 'PUT',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
+        ...options
     };
     const response = await fetch(url, requestOptions);
     return handleResponse(response);
 }
 
 // prefixed with underscored because delete is a reserved word in javascript
-const _delete = async (url: string) => {
+const _delete = async (url: string, options?: RequestInit) => {
     const requestOptions = {
-        method: 'DELETE'
+        method: 'DELETE',
+        ...options
     };
     const response = await fetch(url, requestOptions);
     return handleResponse(response);
 }
 
 
-const useFetch = {
+const fetcher = {
     get,
     post,
     put,
     delete: _delete
 };
 
-export default useFetch;
+export default fetcher;
