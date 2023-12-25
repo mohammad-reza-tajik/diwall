d5dbca986e6167ea75dbaeb69e08d5c5ea4c31b6
Morta <mr.tajik1999@gmail.com>
2023-12-25 10:03:28 +0330

    refactor(validateToken): make the function async and more robust

--- utilities/validateToken.ts
+++ utilities/validateToken.ts
@@ -1,13 +1,11 @@
 import jwt from "jsonwebtoken";
 
+import {promisify} from "util";
 
-const validateToken =  (token : string) => {
-    try {
-        jwt.verify(token, "TheLanternIsTooBright")
-        return true
-    } catch (e) {
-        return false
-    }
+const promisifiedVerify = promisify<string, jwt.Secret>(jwt.verify);
+
+const validateToken = async (token: string) => {
+    return await promisifiedVerify(token, "TheLanternIsTooBright") as unknown as Promise<unknown> as Promise<jwt.JwtPayload>;
 }
 
 export default validateToken
