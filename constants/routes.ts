37e161a8b36b5bdec323c483e17cab6fd1d80f2f
Morta <mr.tajik1999@gmail.com>
2023-12-28 17:59:18 +0330

    fix: Header and Footer not showing in products/:slug page

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,2 +1,2 @@
-const routes = [/\/$/i,/\/auth/i,/\/accounts\/[A-Za-z0-9]+/i,/\/products$/i,/\/products\/[A-Za-z0-9]+/i ];
+const routes = [/\/$/i,/\/auth/i,/\/accounts\/[A-Za-z0-9]+/i,/\/products$/i,/\/products\/%([A-Za-z0-9]+(%[A-Za-z0-9]+)+)/i ];
 export default routes
