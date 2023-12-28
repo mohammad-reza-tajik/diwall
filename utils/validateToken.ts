19b424934a30fd7dfa2b2c2cf73528f851c31bf9
Morta <mr.tajik1999@gmail.com>
2023-12-28 16:09:18 +0330

    perf(validateToken): remove excessive await in the return statement

--- utils/validateToken.ts
+++ utils/validateToken.ts
@@ -5,7 +5,7 @@ import {promisify} from "util";
 const promisifiedVerify = promisify<string, jwt.Secret>(jwt.verify);
 
 const validateToken = async (token: string) => {
-    return await promisifiedVerify(token, "TheLanternIsTooBright") as unknown as Promise<unknown> as Promise<jwt.JwtPayload>;
+    return promisifiedVerify(token, "TheLanternIsTooBright") as unknown as Promise<unknown> as Promise<jwt.JwtPayload>;
 }
 
 export default validateToken
