770e384f18a87bff3aa6159b23769fc9f22230e2
Morta <mr.tajik1999@gmail.com>
2023-12-31 10:04:12 +0330

    style(BannerDesktop): add small horizontal padding

--- components/HomePage/Banner/BannerDesktop.tsx
+++ components/HomePage/Banner/BannerDesktop.tsx
@@ -2,18 +2,17 @@ import Image from "next/image";
 import Link from "next/link";
 
 function BannerDesktop () {
-
     return (
-        <div className={"hidden md:grid grid-cols-6 mb-10"}>
-            <div className={"flex flex-col justify-center gap-8 lg:gap-14 py-4 lg:py-8 bg-primary col-span-2 px-5"}>
-                <h1 className={"text-base lg:text-xl text-white font-dana-black"}>
+        <div className={"hidden md:grid grid-cols-6 mb-10 px-1"}>
+            <div className={"flex flex-col justify-center gap-8 lg:gap-12 py-4 lg:py-8 bg-primary col-span-2 px-5"}>
+                <h1 className={"text-bas lg:text-xl text-white font-dana-black"}>
                     خانه رویایی خود را به واقعیت تبدیل کنید
                 </h1>
-                <p className={"text-justify text-white lg:leading-8 text-xs lg:text-base"}>
+                <p className={"text-justify text-white lg:leading-8 text-xs lg:text-sm xl:text-base"}>
                     از بین هزاران طرح کاغذ و پوستر دیواری فروشگاه دیوال برای فضای خانه و محل کار خود انتخاب و به
                     آسانی آنرا سفارشی کرده وآنلاین تحویل بگیرید
                 </p>
-                <Link className={"btn btn-sm lg:btn-md btn-ghost text-white border border-white"} href={"/products"} aria-label="مشاهده محصولات">
+                <Link className={"btn btn-sm lg:btn-md !text-xs lg:!text-sm btn-ghost text-white border border-white hover:border-white"} href={"/products"} aria-label="مشاهده محصولات">
                     مشاهده محصولات
                 </Link>
             </div>
--- components/HomePage/Banner/index.tsx
+++ components/HomePage/Banner/index.tsx
@@ -2,7 +2,6 @@ import BannerMobile from "./BannerMobile";
 import BannerDesktop from "./BannerDesktop";
 
 function Banner () {
-
     return (
             <section>
                 <BannerMobile/>
