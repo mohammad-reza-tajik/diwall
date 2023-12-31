ea6ec1e4a8c9c3fb57a028e58d8afd18b380d258
Morta <mr.tajik1999@gmail.com>
2023-12-31 10:02:54 +0330

    ref(MainNav): adapt to the new font size

--- components/Globals/MainNav.tsx
+++ components/Globals/MainNav.tsx
@@ -5,11 +5,11 @@ import navLinks from "@/constants/navLinks";
 function MainNav() {
 
     return (
-        <div className={"hidden md:flex justify-between"}>
-            <nav className={"flex items-center gap-10 md:text-xl lg:text-2xl"}>
+        <div className={"hidden md:flex justify-between px-1"}>
+            <nav className={"flex items-center gap-6 text-sm lg:text-base"}>
                 {
                     navLinks.map((link , index) => (
-                        <Link key={index} href={link.href} className={"flex items-center gap-2 hover:text-primary transition-colors"}>
+                        <Link key={index} href={link.href} className={"flex items-center gap-1 hover:text-primary transition-colors"}>
                             <link.icon />
                             {link.label}
                         </Link>
@@ -19,8 +19,8 @@ function MainNav() {
             </nav>
             <div className={"hidden lg:flex"}>
                 <div className={"flex flex-col"}>
-                    <span className={"text-2xl font-dana-bold"}>021464879</span>
-                    <span className={"text-xl text-center"}>پشتیبانی</span>
+                    <span className={"text-base font-dana-bold"}>021464879</span>
+                    <span className={"text-sm text-center"}>پشتیبانی</span>
                 </div>
                 <div className={"flex justify-center items-center"}>
                     <PhoneIcon sx={{fontSize: 30}} color={"primary"}/>
