bcff975f0124c3c5b4791b92f10e3ecf3783cb1b
Morta <mr.tajik1999@gmail.com>
2023-12-31 19:41:07 +0330

    ref(Product): migrate to tailwind

--- components/Globals/Product.tsx
+++ components/Globals/Product.tsx
@@ -1,43 +1,11 @@
 "use client"
-import React, {useState} from "react";
+import {useState} from "react";
 import Link from "next/link";
 import Image from "next/legacy/image"
 import {useRouter} from "next/navigation";
-import Grid from "@mui/material/Grid";
 import {userActions, useAppDispatch, useAppSelector} from "@/store";
-import CircularProgress from "@mui/material/CircularProgress";
-import IconButton from "@mui/material/IconButton";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import type {SxProps} from "@mui/material/styles";
 import type {ProductType} from "@/db/productModel";
-
-const styles = {
-    product: {
-        borderRadius: 1,
-        justifyContent: "center",
-        p: {xs: 7, md: 10},
-        bgcolor: "white.main",
-    },
-    title: {
-        fontSize: {xs: 10, md: 12, lg: 14},
-        fontFamily: "dana-bold",
-        height: "6rem",
-        pt: "1rem",
-    },
-    price: {
-        fontSize: {xs: 10, md: 12, lg: 14},
-        color: "primary.main",
-    },
-    addToWishlistButton: {
-        position: "absolute",
-        left: 5,
-        top: 5,
-        zIndex: 20,
-        bgcolor: "rgba(50,50,50,0.3)",
-        borderRadius: 1,
-    }
-} satisfies Record<string, SxProps>
-
+import {Heart, HeartOutlined} from "@/components/Globals/Icons";
 
 function Product(product: ProductType) {
     const router = useRouter();
@@ -53,44 +21,30 @@ function Product(product: ProductType) {
     }
 
     return (
-        <Grid container item sx={styles.product} xs={12}>
-            <Grid item xs={12} position={"relative"}>
-                <Grid item sx={styles.addToWishlistButton}>
-                    <IconButton onClick={wishlistHandler} aria-label="add to wishlist" disabled={addToWishlistLoading}>
-                        {
-                            addToWishlistLoading ?
-                                <CircularProgress color={"white"} sx={{
-                                    maxWidth: {xs: 20, sm: 25},
-                                    maxHeight: {xs: 20, sm: 25}
-                                }}/> :
-                                isInWishlist ?
-                                    <FavoriteIcon sx={{
-                                        fontSize: {xs: 20, sm: 25},
-                                        color: "primary.main"
-                                    }}/> :
-                                    <FavoriteIcon sx={{
-                                        fontSize: {xs: 20, sm: 25,},
-                                        color: "white.main"
-                                    }}/>
-                        }
-                    </IconButton>
-                </Grid>
-
-                <Link href={`/products/${product.slug}`}>
-                    <Image src={`/assets/pictures/products/${product.slug}.jpg`} alt={`${product.title}`} width={400} height={400}/>
-                </Link>
-            </Grid>
-
-            <Grid container item xs={12} component={Link} href={`/products/${product.slug}`} sx={styles.title}>
+        <div className={"bg-white rounded flex flex-col p-1 md:p-2 relative gap-2 min-h-full"}>
+            <button
+                className={`absolute left-3 top-3 z-20 rounded btn btn-square btn-sm md:btn-md btn-ghost bg-black/30 ${addToWishlistLoading && "btn-disabled"}`}
+                onClick={wishlistHandler} aria-label="add to wishlist" disabled={addToWishlistLoading}>
+                {
+                    addToWishlistLoading ?
+                        <span className={"loading loading-spinner text-white"}></span> :
+                        isInWishlist ?
+                            <Heart className={"fill-primary size-5 md:size-6"}/> :
+                            <HeartOutlined className={"fill-white size-5 md:size-6"}/>
+                }
+            </button>
+            <Link href={`/products/${product.slug}`}>
+                <Image src={`/assets/pictures/products/${product.slug}.jpg`} alt={product.title} width={400} height={400}/>
+            </Link>
+            <Link className={"h-[3rem] font-dana-bold text-xs md:text-sm text-gray-600"} href={`/products/${product.slug}`}>
                 {product.title}
-            </Grid>
-
-            <Grid container item xs={12} component={"span"} sx={styles.price}>
+            </Link>
+            <span className={"text-primary text-xs md:text-sm"}>
                 {product.price + " تومان هر متر مربع"}
-            </Grid>
+            </span>
 
-        </Grid>
+        </div>
     )
 }
 
-export default React.memo(Product)
+export default Product
