c19d15b91d170a1c624a4dc7c58475d10d98d0de
Morta <mr.tajik1999@gmail.com>
2024-01-01 13:04:33 +0330

    feat(Info): migrate to tailwind

--- components/DetailPage/Info.tsx
+++ components/DetailPage/Info.tsx
@@ -1,76 +1,64 @@
 "use client"
 import {useState} from "react";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-import Grid from "@mui/material/Grid";
-import TabPanel from "../Globals/TabPanel";
 import dynamic from "next/dynamic";
 import ReviewsForm from "./ReviewsForm";
 import SwiperProducts from "../Globals/SwiperProducts";
 import type {ProductType} from "@/db/productModel";
-import type {SxProps} from "@mui/material/styles";
-import ReviewsIcon from "@mui/icons-material/Reviews";
-import CreateIcon from "@mui/icons-material/Create";
-import InventoryIcon from '@mui/icons-material/Inventory';
-import {useTheme} from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import {Create, Package, Review} from "@/components/Globals/Icons";
 
 const Reviews = dynamic(() => import("./Reviews"));
 
-const styles = {
-    tab: {
-        fontSize: {xs: 12, md: 15},
-        color: "#555",
-        gap: "1rem"
-    },
-    tabsContainer: {
-        bgcolor: "white.main",
-        borderRadius: 1,
-        fontFamily: "dana-bold",
-    }
-} satisfies Record<string, SxProps>
 
 interface Props {
-   slug: string;
+    slug: string;
     relatedProducts: ProductType[];
 }
 
-function Info({slug , relatedProducts}: Props) {
+function Info({slug, relatedProducts}: Props) {
 
-    const [tab, setTab] = useState(0);
     const [addReview, setAddReview] = useState(false);
+    const [activeTab, setActiveTab] = useState(0);
 
-    const changeTabHandler = (_, newValue: number) => {
-        setTab(newValue);
-    };
-
-    const theme = useTheme();
-    const matchesMD = useMediaQuery(theme.breakpoints.up("md"));
+    const tabs = [
+        {
+            label: "محصولات مشابه",
+            icon: <Package className={"size-6"}/>,
+            content: <SwiperProducts products={relatedProducts}/>
+        },
+        {
+            label: "دیدگاه ها",
+            icon: <Review className={"size-6"}/>,
+            content: <Reviews addReview={addReview} slug={slug}/>
+        },
+        {
+            label: "درج دیدگاه",
+            icon: <Create className={"size-6"}/>,
+            content: <ReviewsForm setAddReview={setAddReview} slug={slug}/>
+        },
+    ];
 
     return (
-        <Grid container my={30}>
-            <Grid item xs={12}>
-                <Tabs value={tab} onChange={changeTabHandler} sx={styles.tabsContainer}>
-                    <Tab icon={<InventoryIcon/>} label={"محصولات مشابه"} iconPosition={matchesMD ? "start" : "top"}
-                         sx={styles.tab}/>
-                    <Tab icon={<ReviewsIcon/>} iconPosition={matchesMD ? "start" : "top"} label={"دیدگاه ها"}
-                         sx={styles.tab}/>
-                    <Tab icon={<CreateIcon/>} label={"درج دیدگاه"} iconPosition={matchesMD ? "start" : "top"}
-                         sx={styles.tab}/>
-                </Tabs>
-            </Grid>
-            <Grid item xs={12} py={30}>
-                <TabPanel index={0} tab={tab}>
-                    <SwiperProducts products={relatedProducts}/>
-                </TabPanel>
-                <TabPanel index={1} tab={tab}>
-                    <Reviews addReview={addReview} slug={slug} />
-                </TabPanel>
-                <TabPanel index={2} tab={tab}>
-                    <ReviewsForm setAddReview={setAddReview} slug={slug}/>
-                </TabPanel>
-            </Grid>
-        </Grid>
+        <section className={"flex flex-col gap-5 my-10"}>
+            <div role="tablist" className="flex bg-white px-2 rounded text-xs md:text-sm">
+                {
+                    tabs.map((tab, index) => (
+                        <button
+                            key={index}
+                            role={"tab"}
+                            className={`flex max-md:flex-col items-center border-b-2 border-b-transparent hover:bg-black/5 transition-all px-3 py-5 font-dana-bold gap-2 ${activeTab === index && "!border-primary text-primary fill-primary"}`}
+                            onClick={() => setActiveTab(index)}
+                        >
+                            {tab.icon}
+                            {tab.label}
+                        </button>
+                    ))
+                }
+            </div>
+            <div role="tabpanel" className={"max-md:px-2"}>
+                {tabs[activeTab].content}
+            </div>
+        </section>
+
     )
 }
 
