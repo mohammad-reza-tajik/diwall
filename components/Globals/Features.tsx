6f819d1e28664e367f770ef182b0efe7c7b1052c
Morta <mr.tajik1999@gmail.com>
2024-01-01 10:20:43 +0330

    ref(Features): migrate to tailwind

--- components/Globals/Features.tsx
+++ components/Globals/Features.tsx
@@ -1,76 +1,26 @@
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import HeadsetMicOutlined from "@mui/icons-material/HeadsetMicOutlined";
-import Inventory2Outlined from "@mui/icons-material/Inventory2Outlined";
-import LocalShippingOutlined from "@mui/icons-material/LocalShippingOutlined";
-import WorkspacePremiumOutlined from "@mui/icons-material/WorkspacePremiumOutlined";
-import React from "react"
-
+import features from "@/constants/features";
 
 function Features () {
-
     return (
-        <Grid container component={"section"} spacing={20}>
-            <Grid container item xs={6} lg={3} gap={10} justifyContent={"space-evenly"} alignItems={"center"}>
-                <Grid container item xs={12} md={"auto"} justifyContent={{xs:"center" , md : "flex-start"}}>
-                    <LocalShippingOutlined color={"primary"} sx={{fontSize: {xs: 35, sm: 40, md: 45}}}/>
-                </Grid>
-                <Grid container item xs={12} md={"auto"} direction={"column"} alignItems={"center"}>
-                    <Typography variant={"caption"} sx={{fontSize: {xs: 12, sm: 14, lg: 18}}}
-                                fontFamily={"dana-black"}>ارسال <Grid component={"span"}
-                                                                      sx={{color: "primary.main"}}>رایگان</Grid></Typography>
-
-                    <Typography variant={"body1"} textAlign={"center"} fontFamily={"dana-bold"}
-                                sx={{fontSize: {xs: 10, md: 12}}}>
-                        ارسال رایگان به تمام نقاط ایران
-                    </Typography>
-
-                </Grid>
-            </Grid>
-            <Grid container item xs={6} lg={3} gap={10} justifyContent={"space-evenly"} alignItems={"center"}>
-                <Grid container item xs={12} md={"auto"} justifyContent={{xs:"center" , md : "flex-start"}}>
-                    <Inventory2Outlined color={"primary"} sx={{fontSize: {xs: 35, sm: 40, md: 45}}}/>
-                </Grid>
-                <Grid container item xs={12} md={"auto"} direction={"column"} alignItems={"center"}>
-                    <Typography variant={"caption"} sx={{fontSize: {xs: 12, sm: 14, lg: 18}}}
-                                fontFamily={"dana-black"}>بسته بندی <Grid component={"span"}
-                                                                          sx={{color: "primary.main"}}>رایگان</Grid></Typography>
-                    <Typography variant={"body1"} textAlign={"center"} fontFamily={"dana-bold"}
-                                sx={{fontSize: {xs: 10, md: 12}}}>
-                        بسته بندی امن برای حفاظت فیزیکی
-                    </Typography>
-                </Grid>
-            </Grid>
-            <Grid container item xs={6} lg={3} gap={10} justifyContent={"space-evenly"} alignItems={"center"}>
-                <Grid container item xs={12} md={"auto"} justifyContent={{xs:"center" , md : "flex-start"}}>
-                    <WorkspacePremiumOutlined color={"primary"} sx={{fontSize: {xs: 35, sm: 40, md: 45}}}/>
-                </Grid>
-                <Grid container item xs={12} md={"auto"} direction={"column"} alignItems={"center"}>
-                    <Typography variant={"caption"} sx={{fontSize: {xs: 12, sm: 14, lg: 18}}}
-                                fontFamily={"dana-black"}>تضمین <Grid component={"span"}
-                                                                      sx={{color: "primary.main"}}>کیفیت</Grid></Typography>
-                    <Typography variant={"body1"} textAlign={"center"} fontFamily={"dana-bold"}
-                                sx={{fontSize: {xs: 10, md: 12}}}>تضمین کیفیت و گارانتی
-                        محصولات</Typography>
-
-                </Grid>
-            </Grid>
-            <Grid container item xs={6} lg={3} gap={10} justifyContent={"space-evenly"} alignItems={"center"}>
-                <Grid container item xs={12} md={"auto"} justifyContent={{xs:"center" , md : "flex-start"}}>
-                    <HeadsetMicOutlined color={"primary"} sx={{fontSize: {xs: 35, sm: 40, md: 45}}}/>
-                </Grid>
-                <Grid container item xs={12} md={"auto"} direction={"column"} alignItems={"center"}>
-                    <Typography variant={"caption"} sx={{fontSize: {xs: 12, sm: 14, lg: 18}}}
-                                fontFamily={"dana-black"}>پشتیبانی <Grid component={"span"}
-                                                                         sx={{color: "primary.main"}}>آنلاین</Grid></Typography>
-                    <Typography variant={"body1"} textAlign={"center"} fontFamily={"dana-bold"}
-                                sx={{fontSize: {xs: 10, md: 12}}}>
-                        پشتیبانی به صورت شبانه روزی
-                    </Typography>
-
-                </Grid>
-            </Grid>
-        </Grid>
+        <section className={"flex flex-wrap"}>
+            {
+                features.map((feature , index) => {
+                    return (
+                        <div className={"flex w-1/2 lg:w-1/4 gap-5 justify-evenly items-center"} key={index}>
+                            {feature.icon}
+                            <div className={"flex flex-col items-center gap-2"}>
+                                <span className={"font-dana-black text-sm sm:text-base lg:text-lg"}>
+                                    {feature.title}
+                                </span>
+                                <p className={"font-dana-bold text-center text-xs md:text-sm"}>
+                                    {feature.description}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </section>
     )
 }
 
---
+++ constants/features.tsx
@@ -0,0 +1,26 @@
+import {Package , Medal , Shipping , HeadPhones} from "@/components/Globals/Icons";
+
+const features = [
+    {
+        title : "ارسال رایگان",
+        description : "ارسال رایگان به تمام نقاط ایران",
+        icon : <Shipping className={"size-10 fill-primary"} />
+    },
+    {
+        title : "بسته بندی رایگان",
+        description : "بسته بندی امن برای حفاظت فیزیکی",
+        icon : <Package className={"size-[2rem] md:size-[4rem] fill-primary"} />
+    },
+    {
+        title : "تضمین کیفیت",
+        description : "تضمین کیفیت و گارانتی محصولات",
+        icon : <Medal className={"!size-[2rem] md:!size-[4rem] fill-primary"} />
+    },
+    {
+        title : "پشتیبانی آنلاین",
+        description : "پشتیبانی به صورت شبانه روزی",
+        icon : <HeadPhones className={"size-8 fill-primary"} />
+    }
+]
+
+export default features;
