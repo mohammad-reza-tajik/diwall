2a9197fc90ea805a8ea84306279cfc0c18aaa4eb
Morta <mr.tajik1999@gmail.com>
2024-01-01 10:17:28 +0330

    ref(HeaderDesktop): put auth button in a component

---
+++ components/Globals/Header/AuthButton.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import {Login} from "@/components/Globals/Icons";
+import { useState} from "react";
+import {UserType} from "@/db/userModel";
+import UserMenu from "@/components/Globals/Header/UserMenu";
+
+function AuthButton(user: UserType) {
+    const [openMenu, setOpenMenu] = useState(false)
+
+    return (
+        user?.username === null ?
+            <Link className={"btn btn-primary w-48 py-2 rounded-full"} href={"/auth"} aria-label="login/signup">
+                <Login className={"fill-white size-6"}/>
+                ورود / ثبت نام
+            </Link> :
+            <button className={"btn btn-primary w-48 relative py-2 rounded-full"} aria-label="show menu"
+                    onClick={() => setOpenMenu((prevState)=>!prevState)}
+            >
+                {user?.username}
+                <UserMenu user={user} open={openMenu} device={"desktop"}/>
+            </button>
+
+
+    )
+}
+
+export default AuthButton;
