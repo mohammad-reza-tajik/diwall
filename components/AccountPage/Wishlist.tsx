0d0c7efa725376ba58e3e16bc499863c71ec738b
Morta <mr.tajik1999@gmail.com>
2024-01-03 20:58:19 +0330

    ref(Wishlist): migrate to tailwind

--- components/AccountPage/Wishlist.tsx
+++ components/AccountPage/Wishlist.tsx
@@ -1,25 +1,21 @@
-import React from "react";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Product from "../Globals/Product";
+import Product from "@/components/Globals/Product";
 import {UserType} from "@/db/userModel";
 
 function Wishlist(user: UserType) {
 
     return (
-        <Grid container item xs={12} spacing={10} height={1}>
+        <>
             {
                 !user.username || user.wishlist.length === 0 ?
-                    <Grid container item xs={12} component={Typography} justifyContent={"center"} fontSize={16}
-                          alignItems={"center"}>
+                    <p className={"text-center flex justify-center items-center col-span-3 text-sm md:text-base"}>
                         لیست علاقمندی های شما خالی است!
-                    </Grid> :
-                    user.wishlist.map(product =>
-                        <Grid item xs={6} sm={4} key={product._id}>
-                            <Product  {...product} />
-                        </Grid>)
+                    </p> :
+                    user.wishlist.map(product => (
+                        <Product key={product._id}  {...product} />
+                    ))
+
             }
-        </Grid>
+        </>
     )
 
 }
