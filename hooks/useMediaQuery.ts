ba6f8c4872b00718fb38712835b6f345ca1c2bbd
Morta <mr.tajik1999@gmail.com>
2024-01-03 09:49:58 +0330

    feat(hooks): add media query hook

---
+++ hooks/useMediaQuery.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+const useMediaQuery = (width:string | number , mode : "up" | "down" = "up") => {
+    let query : string;
+
+    if (mode === "up"){
+        query = `(min-width: ${width}px)`
+    } else {
+        query = `(max-width: ${width}px)`
+    }
+
+    const [isClient, setIsClient] = useState(false);
+    const [matches, setMatches] = useState(isClient ?window.matchMedia(query).matches : true);
+
+    const listener = (event : MediaQueryListEvent) => {
+        setMatches(event.matches);
+    };
+
+    // Add the listener to the media query and remove it on cleanup
+    useEffect(() => {
+        setIsClient(true);
+        const mediaQuery = window.matchMedia(query);
+        mediaQuery.addEventListener("change", listener);
+        return () => mediaQuery.removeEventListener("change", listener);
+    }, [query]);
+
+    // Return the state
+    return matches;
+}
+
+export default useMediaQuery;
