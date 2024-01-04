04473c5c970fe610863bce5380d228d4177102bd
Morta <mr.tajik1999@gmail.com>
2024-01-05 01:16:06 +0330

    ref(SearchResults): migrate to tailwind

--- components/Globals/SearchResults.tsx
+++ components/Globals/SearchResults.tsx
@@ -1,34 +1,14 @@
 "use client"
-import {FormEvent} from "react";
-import Grid from "@mui/material/Grid";
-import CircularProgress from "@mui/material/CircularProgress";
+import {FormEventHandler} from "react";
 import Image from "next/image";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import {useRouter} from "next/navigation";
 import type {ProductType} from "@/db/productModel";
-import type {SxProps} from "@mui/material/styles";
-
-const styles = {
-    searchResultsContainer: {
-        bgcolor: "background.paper",
-        border: {xs: "none", md: "1px solid #ccc"},
-        borderTop: "none",
-        flexDirection: "column",
-        p: ".5rem"
-    },
-    item: {
-        gap: 10,
-        p: 5,
-    }
-} satisfies Record<string, SxProps>
 
 interface Props {
     isLoading: boolean;
     results: ProductType[];
     search: string;
-    // eslint-disable-next-line no-unused-vars
-    submitSearchHandler: (event: FormEvent) => void
+    submitSearchHandler: FormEventHandler
     closeSearch: () => void;
 
 }
@@ -43,58 +23,50 @@ function SearchResults({isLoading, results, search, submitSearchHandler, closeSe
 
     }
 
-    return (
-        <Grid container item xs={12} sx={styles.searchResultsContainer}>
-            {
-                isLoading ?
-                    <Grid container justifyContent={"center"} alignItems={"center"}>
-                        <CircularProgress color={"primary"} size={45}/>
-                    </Grid> :
-                    <>
-                        <Grid component={"ul"} container item xs={12}>
-                            {results.map((result) => {
-                                if (search.trim().length >= 3 && results.length !== 0) {
-                                    return (
-                                        <Grid container item alignItems={"center"} xs={12} key={result._id}
-                                              onClick={() => goToProductHandler(result.slug)}
-                                              sx={styles.item} className={"pointer"}>
+    const showResults = () => {
+        if (isLoading) {
+            return <span className={"loading loading-spinner text-primary"}></span>
+        } else {
+            if (results.length === 0 && search.trim().length >= 3) {
+                return (
+                    <p className={"flex justify-center items-center"}>
+                        نتیجه ای یافت نشد!
+                    </p>
+                )
 
-                                            <Grid item xs={"auto"}>
-                                                <Image
-                                                    src={`/assets/pictures/products/${result.slug}.jpg`}
-                                                    alt={result.title}
-                                                    width={50}
-                                                    height={50}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={true}>
-                                                <Typography variant={"h5"} fontSize={"1.2rem"}>
-                                                    {result.title}
-                                                </Typography>
+            } else if (search.trim().length >= 3 && results.length !== 0) {
+                return results.map((result) => {
+                    return (
+                        <li key={result._id}>
+                            <button className={"btn btn-ghost items-center w-full"} onClick={() => goToProductHandler(result.slug)}>
+                            <Image
+                                src={`/assets/pictures/products/${result.slug}.jpg`}
+                                alt={result.title}
+                                width={50}
+                                height={50}
+                            />
+                            {result.title}
+                            </button>
+                        </li>
 
-                                            </Grid>
-                                        </Grid>
-                                    )
-                                }
-                            })}
-                            {results.length === 0 && search.trim().length >= 3 && !isLoading && (
-                                <Grid container justifyContent={"center"} alignItems={"center"} p={10}>
-                                    <Typography variant={"h5"} fontSize={16}>
-                                        نتیجه ای یافت نشد!
-                                    </Typography>
-                                </Grid>
-                            )
-                            }
-                            {results.length !== 0 && search.trim() !== "" &&
-                                <Button variant={"contained"} onClick={submitSearchHandler}
-                                        sx={{width: 1, fontSize: 14, mt: 15}}>
-                                    مشاهده بیشتر
-                                </Button>
-                            }
-                        </Grid>
-                    </>
+                    )
+
+                })
             }
-        </Grid>
+        }
+    }
+
+
+    return (
+        <ul className="menu bg-white w-full gap-2 shadow absolute z-50 top-full right-0 p-1 text-gray-600 text-xs md:text-sm rounded">
+            {showResults()}
+            {results.length !== 0 && search.trim() !== "" &&
+                <button className={"btn btn-primary btn-sm rounded-full"} onClick={submitSearchHandler}>
+                    مشاهده بیشتر
+                </button>
+            }
+        </ul>
+
     )
 }
 
