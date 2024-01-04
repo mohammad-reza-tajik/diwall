d31a44253ef7d85f3cfc91026c42f7eb07d9ae1e
Morta <mr.tajik1999@gmail.com>
2024-01-04 17:12:51 +0330

    ref(account page): migrate to tailwind

--- app/accounts/[userId]/page.tsx
+++ app/accounts/[userId]/page.tsx
@@ -1,106 +1,82 @@
 "use client"
 import {useSearchParams} from "next/navigation";
-import Grid from "@mui/material/Grid";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import {useTheme} from "@mui/material/styles";
 import {useEffect, useState} from "react";
-import TabPanel from "@/components/Globals/TabPanel";
 import {useAppSelector} from "@/store";
 import dynamic from "next/dynamic";
-import type {SxProps} from "@mui/material/styles";
-import Favorite from "@mui/icons-material/Favorite";
-import Person from "@mui/icons-material/Person";
-import ShoppingBag from "@mui/icons-material/ShoppingBag";
-
-// import Moderation from "@/components/AccountPage/Moderation";
+import {HeartOutlined, ShoppingBag, Person} from "@/components/Globals/Icons";
+import formUrlQuery from "@/utils/formUrlQuery";
+import {useRouter} from "next/navigation";
 
 const Profile = dynamic(() => import("@/components/AccountPage/Profile"));
-const AddProduct = dynamic(() => import("@/components/AccountPage/AddProduct"));
 const Wishlist = dynamic(() => import("@/components/AccountPage/Wishlist"));
 const Cart = dynamic(() => import("@/components/AccountPage/Cart"));
-
-const styles = {
-    tab: {
-        fontSize: {xs: 12, md: 14},
-        color: "#555",
-        gap: "1rem"
-    },
-    tabsContainer: {
-        bgcolor: "white.main",
-        borderRadius: 1,
-        fontFamily: "dana-bold",
-    }
-} satisfies Record<string, SxProps>
-
 
 function AccountPage() {
 
     const query = useSearchParams();
+    const router = useRouter();
 
     const user = useAppSelector(state => state.user);
 
-    const [tab, setTab] = useState(1);
+    const [activeTab, setActiveTab] = useState(0);
 
     const queryTab = query.get("tab");
 
-    const tabChangeHandler = (_, newTab: number) => {
-        setTab(newTab);
-    }
-
     useEffect(() => {
         if (queryTab) {
-            setTab(Number(queryTab));
+            setActiveTab(Number(queryTab));
         }
     }, [queryTab])
 
 
-    const theme = useTheme();
-    const matchesMD = useMediaQuery(theme.breakpoints.up("md"));
+
+
+    const tabs = [
+        {
+            label: "اطلاعات کاربر",
+            icon: <Person className={"size-4 md:size-5"}/>,
+            content: <Profile {...user}/>
+        },
+        {
+            label: "لیست علاقمندی ها",
+            icon: <HeartOutlined className={"size-4 md:size-5"}/>,
+            content: <Wishlist {...user}/>
+        },
+        {
+            label: "سبد خرید",
+            icon: <ShoppingBag className={"size-4 md:size-5"}/>,
+            content: <Cart {...user}/>
+        },
+    ];
 
     return (
-            <Grid container columns={14} gap={10}>
-                <Grid item xs={14} md={3} sx={styles.tabsContainer}>
-                    <Tabs onChange={tabChangeHandler} value={tab}
-                          orientation={matchesMD ? "vertical" : "horizontal"}>
-                        <Tab icon={<Person/>} iconPosition={matchesMD ? "start" : "top"} label="اطلاعات کاربر"
-                             sx={styles.tab}/>
-                        <Tab icon={<Favorite/>} iconPosition={matchesMD ? "start" : "top"} label="لیست علاقمندی ها"
-                             sx={styles.tab}/>
-                        <Tab icon={<ShoppingBag/>} iconPosition={matchesMD ? "start" : "top"} label="سبد خرید"
-                             sx={styles.tab}/>
-                        {user.role === "admin" && <Tab label="افزودن محصول" sx={styles.tab}/>}
-                        {/*{user.role === "admin" && <Tab label="بررسی دیدگاه ها" value={5} sx={styles.tab}/>}*/}
-                    </Tabs>
-
-                </Grid>
-                <Grid item xs={14} md minHeight={300}>
-
-                    <TabPanel tab={tab} index={0}>
-                        <Profile {...user}/>
-                    </TabPanel>
-
-                    <TabPanel tab={tab} index={1}>
-                        <Wishlist {...user}/>
-                    </TabPanel>
-
-                    <TabPanel tab={tab} index={2}>
-                        <Cart {...user}/>
-                    </TabPanel>
-
-                    {user.role === "admin" &&
-                        <>
-                            <TabPanel tab={tab} index={3}>
-                                <AddProduct/>
-                            </TabPanel>
-                            {/*<TabPanel tab={tab} index={4}>
-                                <Moderation/>
-                            </TabPanel>*/}
-                        </>
+            <section className={"flex flex-col md:flex-row gap-2 my-10"}>
+                <div role="tablist" className="flex flex-row basis-5/5 md:basis-1/5 md:flex-col md:py-4 bg-white rounded text-xs sm:text-sm gap-2">
+                    {
+                        tabs.map((tab, index) => (
+                            <button
+                                key={index}
+                                role={"tab"}
+                                className={`flex justify-center max-md:flex-col items-center max-md:border-b-2 md:border-r-2 border-transparent hover:bg-black/5 transition-all max-md:px-3 py-2 md:py-4 font-dana-bold gap-2 ${activeTab === index && "!border-primary text-primary fill-primary"}`}
+                                onClick={() => {
+                                    setActiveTab(index);
+                                    router.push(formUrlQuery(query.toString(), {
+                                        params: {
+                                            tab:index
+                                        }
+                                    }))
+                                }}
+                            >
+                                {tab.icon}
+                                {tab.label}
+                            </button>
+                        ))
                     }
-                </Grid>
-            </Grid>
+                </div>
+                <div role="tabpanel" className={"basis-4/5"}>
+                    {tabs[activeTab].content}
+                </div>
+            </section>
     )
 }
 
