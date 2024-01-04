990ec5e782c7c6fad561407734273d2a4b6409b5
Morta <mr.tajik1999@gmail.com>
2024-01-04 23:53:18 +0330

    ref(detail-page): migrate to tailwind

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,25 +1,24 @@
 "use client"
-import CircularProgress from "@mui/material/CircularProgress";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import SectionHeading from "@/components/Globals/SectionHeading";
 import {useEffect, useState} from "react";
 import {useSearchParams} from "next/navigation";
 import Product from "@/components/Globals/Product";
-import Pagination from "@/components/Globals/Pagination";
 import {getAllProducts} from "@/actions/product";
 import qs from "query-string";
+import Pagination from "@/components/Globals/Pagination";
+import {ProductType} from "@/db/productModel";
 
 
-interface PageInformation {
-    lastPage?: number;
-    currentPage?: number;
+interface Response {
+    products: ProductType[];
+    productsCount: number;
+    currentPage: number;
+    lastPage: number;
 }
 
 function Products() {
 
-    const [products, setProducts] = useState([]);
-    const [pageInformation, setPageInformation] = useState<PageInformation>({});
+    const [data, setData] = useState<Response>();
     const [isLoading, setIsLoading] = useState(true);
 
     const {category, page, sortBy, search} = qs.parse(useSearchParams().toString()) as Record<string, string>;
@@ -29,9 +28,8 @@ function Products() {
         (async () => {
             try {
                 setIsLoading(true);
-                const res = await getAllProducts({category, page: +page, sortBy, search});
-                setProducts(res.products);
-                setPageInformation(res);
+                const res = await getAllProducts({category, page: page ? +page : undefined, sortBy, search});
+                setData(res);
 
             } catch (err) {
                 console.log(err);
@@ -42,51 +40,43 @@ function Products() {
 
         })()
 
-    }, [category , page , sortBy , search]);
+    }, [category, page, sortBy, search]);
 
 
     return (
-        <Grid container item xs={12} direction={"column"}>
-            <Grid item xs>
+        <section className={"flex flex-col"}>
                 {
                     search && <SectionHeading text={`محصولات مرتبط با ${search}`} sortBy={true}/>
                 }
                 {
-                    category &&  <SectionHeading text={`پوستر برای ${category.split("-").join(" ")}`} sortBy={true}/>
+                    category && <SectionHeading text={`پوستر برای ${category.split("-").join(" ")}`} sortBy={true}/>
                 }
                 {
-                    !category && !search && sortBy && <SectionHeading text={`${sortBy.split("-").join(" ")} محصولات`} sortBy={true}/>
+                    !category && !search && sortBy &&
+                    <SectionHeading text={`${sortBy.split("-").join(" ")} محصولات`} sortBy={true}/>
                 }
-
-            </Grid>
-            <Grid container item xs spacing={10} alignItems={"center"}>
+            <div
+                className={`grid grid-cols-2 min-h-[300px] md:grid-cols-3 lg:grid-cols-4 gap-1 ${data?.products.length !== 0 || !isLoading ? "content-start" : "content-center"}`}>
                 {
                     isLoading ?
-                        <Grid container item xs justifyContent={"center"}>
-                            <CircularProgress color={"primary"} size={45}/>
-                        </Grid> :
-                        products.length === 0 ?
-                            <Grid container item xs minHeight={300} justifyContent={"center"} alignItems={"center"}>
-                                <Typography fontSize={20} variant={"h2"} color={"#333"} fontFamily={"dana-bold"}>
-                                    هیچ محصول مرتبطی موجود نیست !
-                                </Typography>
-                            </Grid> :
-                            products.map((product) =>
-
-                                <Grid item xs={6} sm={4} md={3} key={product._id}>
-                                    <Product {...product} />
-                                </Grid>
+                        <span className={"loading loading-spinner text-white"}></span> :
+                        data?.products.length === 0 ?
+                            <p className={"text-lg flex justify-center items-center"}>
+                                هیچ محصول مرتبطی موجود نیست !
+                            </p>
+                            :
+                            data?.products.map((product) =>
+                                <Product key={product._id} {...product} />
                             )}
 
+
+            </div>
                 {
-                    isLoading || products.length === 0 ?
-                        null :
-                        <Pagination {...pageInformation} />
+                    !isLoading || data?.products.length !== 0 ?
+                        <Pagination currentPage={data.currentPage} lastPage={data.lastPage}/> :
+                        null
                 }
-
-
-            </Grid>
-        </Grid>
+        </section>
     )
 }
 
