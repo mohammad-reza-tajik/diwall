2261b538eb1916530825acaa80f476db07caaca4
Morta <mr.tajik1999@gmail.com>
2024-01-05 22:58:28 +0330

    ref(SearchResults): fix responsiveness issues
    made loading spinner centered
    use a div instead of a button as search result items cause buttons would break the layout if the title of the product is longer than one line

--- components/Globals/SearchResults.tsx
+++ components/Globals/SearchResults.tsx
@@ -25,11 +25,12 @@ function SearchResults({isLoading, results, search, submitSearchHandler, closeSe
 
     const showResults = () => {
         if (isLoading) {
-            return <span className={"loading loading-spinner text-primary"}></span>
+            return <div className={"flex w-full justify-center items-center p-3"}><span
+                className={"loading loading-spinner text-primary"}></span></div>
         } else {
             if (results.length === 0 && search.trim().length >= 3) {
                 return (
-                    <p className={"flex justify-center items-center"}>
+                    <p className={"flex p-5 text-base justify-center items-center"}>
                         نتیجه ای یافت نشد!
                     </p>
                 )
@@ -38,15 +39,19 @@ function SearchResults({isLoading, results, search, submitSearchHandler, closeSe
                 return results.map((result) => {
                     return (
                         <li key={result._id}>
-                            <button className={"btn btn-ghost items-center w-full"} onClick={() => goToProductHandler(result.slug)}>
-                            <Image
-                                src={`/assets/pictures/products/${result.slug}.jpg`}
-                                alt={result.title}
-                                width={50}
-                                height={50}
-                            />
-                            {result.title}
-                            </button>
+                            <div className={"flex items-center gap-2"} role={"button"}
+                                 onClick={() => goToProductHandler(result.slug)}>
+                                <Image
+                                    src={`/assets/pictures/products/${result.slug}.jpg`}
+                                    alt={result.title}
+                                    width={50}
+                                    height={50}
+                                    className={"rounded-full size-10"}
+                                />
+                                <span className={"text-xs md:text-sm"}>
+                                    {result.title}
+                                </span>
+                            </div>
                         </li>
 
                     )
@@ -58,10 +63,10 @@ function SearchResults({isLoading, results, search, submitSearchHandler, closeSe
 
 
     return (
-        <ul className="menu bg-white w-full gap-2 shadow absolute z-50 top-full right-0 p-1 text-gray-600 text-xs md:text-sm rounded">
+        <ul className="menu bg-white w-full absolute z-50 top-full right-0 p-1 rounded">
             {showResults()}
             {results.length !== 0 && search.trim() !== "" &&
-                <button className={"btn btn-primary btn-sm rounded-full"} onClick={submitSearchHandler}>
+                <button className={"btn btn-primary btn-sm rounded-full mt-3 w-full"} onClick={submitSearchHandler}>
                     مشاهده بیشتر
                 </button>
             }
