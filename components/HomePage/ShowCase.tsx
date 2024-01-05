a5c9192a9bf7d990e52f116a4cfbe80a1fca61d5
Morta <mr.tajik1999@gmail.com>
2024-01-05 11:29:13 +0330

    ref(ShowCase): migrate to tailwind

--- components/HomePage/ShowCase.tsx
+++ components/HomePage/ShowCase.tsx
@@ -1,43 +1,18 @@
-import Grid from "@mui/material/Grid";
 import Image from "next/legacy/image"
-import type {SxProps} from "@mui/material/styles";
-
-const styles = {
-    picture: {
-        p: {xs: 15, md: 0}
-    }
-} satisfies Record<string, SxProps>
+import showcase from "@/constants/showcase";
 
 function ShowCase() {
-
     return (
-        <Grid container item component={"section"} alignItems={"center"} overflow={"hidden"} xs={12} my={50}
-              border={"1px solid #ddd"} borderRadius={2}>
-            <Grid container justifyContent={"center"} sx={styles.picture} alignItems={"center"} item xs={4} md={2}>
-                <Image src={"/assets/pictures/showcase/digistyle.svg"} alt={"دیجی استایل"}
-                       width={100} height={100}/>
-            </Grid>
-            <Grid container justifyContent={"center"} sx={styles.picture} alignItems={"center"} item xs={4} md={2}>
-                <Image src={"/assets/pictures/showcase/ganjeh.svg"} alt={"گنجه"}
-                       width={100} height={100}/>
-            </Grid>
-            <Grid container justifyContent={"center"} sx={styles.picture} alignItems={"center"} item xs={4} md={2}>
-                <Image src={"/assets/pictures/showcase/fidibo.svg"} alt={"فیدیبو"}
-                       width={100} height={100}/>
-            </Grid>
-            <Grid container justifyContent={"center"} sx={styles.picture} alignItems={"center"} item xs={4} md={2}>
-                <Image src={"/assets/pictures/showcase/komoda.svg"} alt={"کمدا"}
-                       width={100} height={100}/>
-            </Grid>
-            <Grid container justifyContent={"center"} sx={styles.picture} alignItems={"center"} item xs={4} md={2}>
-                <Image src={"/assets/pictures/showcase/pindo.svg"} alt={"پیندو"}
-                       width={100} height={100}/>
-            </Grid>
-            <Grid container justifyContent={"center"} sx={styles.picture} alignItems={"center"} item xs={4} md={2}>
-                <Image src={"/assets/pictures/showcase/magnet.svg"} alt={"مگنت"}
-                       width={100} height={100}/>
-            </Grid>
-        </Grid>
+        <section className={"grid grid-cols-3 md:grid-cols-6 gap-10 p-5 my-10 rounded border"}>
+            {
+                showcase.map((image)=>{
+                    return (
+                            <Image key={image.alt} src={`/assets/pictures/showcase/${image.name}.svg`} alt={image.alt}
+                                   width={100} height={100}/>
+                    )
+                })
+            }
+        </section>
     )
 }
 
---
+++ constants/showcase.ts
@@ -0,0 +1,23 @@
+const showcase = [
+    {
+        name : "ganjeh",
+        alt :"گنجه"
+    },{
+        name : "fidibo",
+        alt :"فیدیبو"
+    },{
+        name : "komoda",
+        alt :"کمدا"
+    },{
+        name : "pindo",
+        alt :"پیندو"
+    },{
+        name : "magnet",
+        alt :"مگنت"
+    },{
+        name : "digistyle",
+        alt :"دیجی استایل"
+    },
+]
+
+export default showcase;
