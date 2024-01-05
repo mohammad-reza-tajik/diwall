37a688b88f8458fc41553c2fc408033e11a3b797
Morta <mr.tajik1999@gmail.com>
2024-01-05 21:27:53 +0330

    style(MenuDrawer): tweak spacing a little bit

--- components/Globals/Header/MenuDrawer.tsx
+++ components/Globals/Header/MenuDrawer.tsx
@@ -1,37 +1,7 @@
 "use client"
-import Grid from "@mui/material/Grid";
-import React from "react";
-import Image from "next/image";
-import Typography from "@mui/material/Typography";
 import {useRouter} from "next/navigation";
-import type {SxProps} from "@mui/material/styles";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import HomeIcon from '@mui/icons-material/Home';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import InfoIcon from '@mui/icons-material/Info';
-import HandshakeIcon from '@mui/icons-material/Handshake';
-import SellIcon from '@mui/icons-material/Sell';
-import MenuList from '@mui/material/MenuList';
-import MenuItem from "@mui/material/MenuItem";
-import Divider from "@mui/material/Divider";
-import Drawer from "react-modern-drawer";
-
-const styles = {
-    drawer: {
-        p: 20,
-        bgcolor: "background.paper",
-        width: {xs: "75vw", sm: "50vw"},
-        height:"100vh"
-    },
-    item: {
-        py: "1rem",
-    },
-    icon: {
-        fontSize: 20,
-    }
-
-} satisfies Record<string, SxProps>
+import Logo from "@/components/Globals/Logo";
+import navLinks from "@/constants/navLinks";
 
 interface Props {
     setOpenMenuDrawer: React.Dispatch<React.SetStateAction<boolean>>;
@@ -39,7 +9,7 @@ interface Props {
 
 }
 
-function MenuDrawer ({openMenuDrawer, setOpenMenuDrawer } : Props) {
+function MenuDrawer({openMenuDrawer, setOpenMenuDrawer}: Props) {
 
     const router = useRouter();
 
@@ -49,58 +19,31 @@ function MenuDrawer ({openMenuDrawer, setOpenMenuDrawer } : Props) {
     }
 
     return (
-     <Drawer open={openMenuDrawer} direction={"right"} lockBackgroundScroll onClose={()=>setOpenMenuDrawer(false)} size={"auto"}>
-            <Grid container direction={"column"} sx={styles.drawer}>
-                <Grid item onClick={() => menuItemsHandler("/")} className={"pointer"}>
-                    <Image src={"/icons/logo.png"} alt={"diwall-logo"}
-                           width={100}
-                           height={100}/>
-                </Grid>
-                <MenuList sx={{width: 1}}>
-                    <MenuItem sx={styles.item} onClick={() => menuItemsHandler("/")}>
-                        <ListItemIcon>
-                            <HomeIcon sx={styles.icon}/>
-                        </ListItemIcon>
-                        <Typography fontSize={12}>صفحه نخست</Typography>
-                    </MenuItem>
-                    <Divider/>
-                    <MenuItem sx={styles.item} onClick={() => menuItemsHandler("/products")}>
-                        <ListItemIcon>
-                            <WhatshotIcon sx={styles.icon}/>
-                        </ListItemIcon>
-                        <Typography fontSize={12}> جدیدترین محصولات</Typography>
-                    </MenuItem>
-                    <Divider/>
-                    <MenuItem sx={styles.item} onClick={() => menuItemsHandler("/products?sortBy=2")}>
-                        <ListItemIcon>
-                            <SellIcon sx={styles.icon}/>
-                        </ListItemIcon>
-                        <Typography fontSize={12}> پرفروش ترین محصولات</Typography>
-                    </MenuItem>
-                    <Divider/>
-                    <MenuItem sx={styles.item} onClick={() => menuItemsHandler("/products?sortBy=3")}>
-                        <ListItemIcon>
-                            <FavoriteIcon sx={styles.icon}/>
-                        </ListItemIcon>
-                        <Typography fontSize={12}>محبوب ترین محصولات</Typography>
-                    </MenuItem>
-                    <Divider/>
-                    <MenuItem sx={styles.item} onClick={() => menuItemsHandler("/about")}>
-                        <ListItemIcon>
-                            <InfoIcon sx={styles.icon}/>
-                        </ListItemIcon>
-                        <Typography fontSize={12}>درباره ما</Typography>
-                    </MenuItem>
-                    <Divider/>
-                    <MenuItem sx={styles.item} onClick={() => menuItemsHandler("/collaboration")}>
-                        <ListItemIcon>
-                            <HandshakeIcon sx={styles.icon}/>
-                        </ListItemIcon>
-                        <Typography fontSize={12}>همکاری با ما</Typography>
-                    </MenuItem>
-                </MenuList>
-            </Grid>
-     </Drawer>
+        <div className="relative">
+            {openMenuDrawer && (
+                <div className="fixed inset-0 bg-gray-900 bg-opacity-50 z-50"
+                     onClick={() => setOpenMenuDrawer(false)}></div>
+            )}
+            <div
+                className={`fixed top-0 right-0 h-full w-3/4 md:w-1/2 bg-white shadow-lg z-50 transform transition-transform ease-in-out duration-300 ${openMenuDrawer ? "translate-x-0" : "translate-x-full"}`}>
+                <div className="flex flex-col gap-2 p-5">
+
+                    <Logo onClick={() => menuItemsHandler("/")} className={"p-4 size-36"}/>
+                    {
+                        navLinks.map((link, index) => {
+                            return (
+                                <button onClick={() => menuItemsHandler(link.href)} key={index}
+                                        className={"flex items-center gap-3 hover:bg-black/5 border-b p-4 hover:fill-primary transition-colors"}>
+                                    {link.icon}
+                                    {link.text}
+                                </button>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+
 
     )
 
