03444fef9a56d7734dafd1dd1df06606c0e7d49b
Morta <mr.tajik1999@gmail.com>
2024-01-06 09:17:36 +0330

    ref(Header): migrate to tailwind

--- components/Globals/Header/index.tsx
+++ components/Globals/Header/index.tsx
@@ -1,25 +1,13 @@
-"use client"
 import HeaderMobile from "./HeaderMobile";
 import HeaderDesktop from "./HeaderDesktop";
-import Grid from "@mui/material/Grid";
-import { usePathname } from "next/navigation";
-import routes from "@/constants/routes";
 
 function Header ()  {
 
-    const pathname = usePathname();
-
-    if (pathname === "auth" || !routes.some(route => route.test(pathname))) return null;
-
     return (
-        <>
-            <Grid component={"header"} display={{xs: "block", md: "none"}}>
+            <header>
                 <HeaderMobile/>
-            </Grid>
-            <Grid component={"header"} display={{xs: "none", md: "block"}}>
                 <HeaderDesktop/>
-            </Grid>
-        </>
+            </header>
 
     )
 
