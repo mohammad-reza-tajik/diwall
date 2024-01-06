8b8dfbd6475a14df077261c45f08bce89d860561
Morta <mr.tajik1999@gmail.com>
2024-01-06 21:10:05 +0330

    ref(products-page): convert to a server component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,14 +1,8 @@
-"use client"
 import SectionHeading from "@/components/Globals/SectionHeading";
-import {useEffect, useState} from "react";
-import {useSearchParams} from "next/navigation";
 import Product from "@/components/Globals/Product";
 import {getAllProducts} from "@/actions/product";
-import qs from "query-string";
 import Pagination from "@/components/Globals/Pagination";
 import {ProductType} from "@/db/productModel";
-import {enqueueSnackbar} from "notistack";
-
 
 interface Response {
     products: ProductType[];
@@ -17,35 +11,14 @@ interface Response {
     lastPage: number;
 }
 
-function Products() {
+async function Products({searchParams}:{searchParams : Record<string, string>}) {
 
-    const [data, setData] = useState<Response>();
-    const [isLoading, setIsLoading] = useState(true);
-
-    const {category, page, sortBy, search} = qs.parse(useSearchParams().toString()) as Record<string, string>;
-
-
-    useEffect(() => {
-        (async () => {
-            try {
-                setIsLoading(true);
-                const res = await getAllProducts({category, page: page ? +page : undefined, sortBy, search});
-                setData(res);
-
-            } catch (err) {
-                enqueueSnackbar(err.message, {variant:"error"});
-            } finally {
-                setIsLoading(false);
-
-            }
-
-        })()
-
-    }, [category, page, sortBy, search]);
-
+    const {category, page, sortBy, search} = searchParams;
+    const data : Response = await getAllProducts({category, page: page ? +page : undefined, sortBy, search});
 
     return (
         <section className={"flex flex-col"}>
+
                 {
                     search && <SectionHeading text={`محصولات مرتبط با ${search}`} sortBy={true}/>
                 }
@@ -57,10 +30,9 @@ function Products() {
                     <SectionHeading text={`${(sortBy || "جدیدترین").split("-").join(" ")} محصولات`} sortBy={true}/>
                 }
             <div
-                className={`grid grid-cols-2 min-h-[300px] md:grid-cols-3 lg:grid-cols-4 gap-1 ${data?.products.length !== 0 || !isLoading ? "content-start" : "content-center"}`}>
+                className={`grid grid-cols-2 min-h-[300px] md:grid-cols-3 lg:grid-cols-4 gap-1 ${data?.products.length !== 0 ? "content-start" : "content-center"}`}>
                 {
-                    isLoading ?
-                        <span className={"loading loading-spinner text-white"}></span> :
+
                         data?.products.length === 0 ?
                             <p className={"text-lg flex justify-center items-center"}>
                                 هیچ محصول مرتبطی موجود نیست !
@@ -69,15 +41,14 @@ function Products() {
                             data?.products.map((product) =>
                                 <Product key={product._id} {...product} />
                             )}
-
-
             </div>
                 {
-                    !isLoading || data?.products.length !== 0 ?
+                    data?.products.length !== 0 ?
                         <Pagination currentPage={data?.currentPage} lastPage={data?.lastPage}/> :
                         null
                 }
         </section>
+
     )
 }
 
