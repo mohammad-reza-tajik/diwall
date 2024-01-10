2519177d5cd59716daec87067ae63521e234957d
Morta <mr.tajik1999@gmail.com>
2024-01-10 20:10:09 +0330

    ref(useSearch): set loading to true after any changes in search field
    previously when  searching  after another search there was a short moment that the previous search results show up before loading spinner

--- hooks/useSearch.ts
+++ hooks/useSearch.ts
@@ -3,63 +3,15 @@ import {useRouter} from "next/navigation";
 import type {ProductType} from "@/db/productModel";
 import {getAllProducts} from "@/actions/product";
 
-
 const useSearch = (device: "desktop" | "mobile", setOpenSearchDrawer?: Dispatch<SetStateAction<boolean>>) => {
 
-    const router = useRouter()
+    const router = useRouter();
 
     const [search, setSearch] = useState("");
     const [isWrong, setIsWrong] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [results, setResults] = useState<ProductType[]>([]);
 
-    const submitSearchHandler = (event : FormEvent) => {
-        event.preventDefault()
-        if (search.trim() === "") {
-            setIsWrong(true)
-            setTimeout(() => {
-                setIsWrong(false)
-            }, 5000)
-            return
-        }
-        setIsWrong(false)
-        router.push(`/products?search=${search}`)
-        if (device === "mobile") {
-            closeSearchHandlerMobile()
-        } else {
-            closeSearchHandlerDesktop()
-        }
-    }
-
-
-    const closeSearchHandlerDesktop = useCallback(() => {
-        setIsWrong(false);
-        setSearch("");
-
-    }, [])
-
-    const closeSearchHandlerMobile = useCallback(() => {
-        setIsWrong(false);
-        setSearch("");
-        setOpenSearchDrawer(false);
-
-    }, [])
-
-    const searchChangeHandler = (event : ChangeEvent<HTMLInputElement>) => {
-        setSearch(event.target.value);
-        optimizedFn(event.target.value);
-        if (event.target.value.trim() === "") {
-            setIsWrong(true);
-            setTimeout(() => {
-                setIsWrong(false);
-            }, 5000)
-
-        } else {
-            setIsWrong(false)
-        }
-    }
-
-    /*** start of debouncing ***/
     const debounce = (func) => {
         let timer;
         return function (...args) {
@@ -81,6 +33,56 @@ const useSearch = (device: "desktop" | "mobile", setOpenSearchDrawer?: Dispatch<
 
     };
     const optimizedFn = useCallback(debounce(handleChange), []);
+
+    const closeSearchHandlerDesktop = useCallback(() => {
+        setIsWrong(false);
+        setSearch("");
+        setResults([]);
+
+    }, [])
+
+    const closeSearchHandlerMobile = useCallback(() => {
+        setIsWrong(false);
+        setSearch("");
+        setResults([]);
+        setOpenSearchDrawer(false);
+
+    }, [])
+
+    const submitSearchHandler = (event : FormEvent) => {
+        event.preventDefault();
+        if (search.trim() === "") {
+            setIsWrong(true)
+            setTimeout(() => {
+                setIsWrong(false)
+            }, 5000)
+            return
+        }
+        setIsWrong(false);
+        if (device === "mobile") {
+            closeSearchHandlerMobile();
+        } else {
+            closeSearchHandlerDesktop();
+        }
+        router.push(`/products?search=${search}`);
+    }
+
+
+    const searchChangeHandler = (event : ChangeEvent<HTMLInputElement>) => {
+        setIsLoading(true);
+        setSearch(event.target.value);
+        optimizedFn(event.target.value);
+        if (event.target.value.trim() === "") {
+            setIsWrong(true);
+            setTimeout(() => {
+                setIsWrong(false);
+            }, 5000)
+
+        } else {
+            setIsWrong(false);
+        }
+    }
+
 
     if (device === "desktop") {
 
