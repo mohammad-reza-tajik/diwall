f3c7e943ae4b5b8b14c9a554372ee64749d26180
Morta <mr.tajik1999@gmail.com>
2024-01-16 07:10:19 +0330

    feat: add Skeleton component from shadcn and use it in loading pages

--- app/products/[slug]/loading.tsx
+++ app/products/[slug]/loading.tsx
@@ -1,42 +1,44 @@
+import {Skeleton} from "@/components/ui/skeleton";
+
 function Loading() {
 
     return (
         <>
             <div className={"flex max-md:flex-col max-md:gap-3"}>
                 <div className={"flex flex-col gap-2 w-full md:w-1/2 md:pl-10"}>
-                    <div className={"skeleton w-full h-80"}/>
-                    <div className={"skeleton w-full h-20 md:h-40"}/>
+                    <Skeleton className={"w-full h-80"}/>
+                    <Skeleton className={"w-full h-20 md:h-40"}/>
                 </div>
                 <div className={"flex flex-col gap-10 max-md:p-1 w-full md:w-1/2"}>
                     <div className={"flex justify-between items-center"}>
-                        <div className={"skeleton w-64 h-5"}/>
-                        <div className={"skeleton w-14 h-5"}/>
+                        <Skeleton className={"w-64 h-5"}/>
+                        <Skeleton className={"w-14 h-5"}/>
                     </div>
-                    <div className={"skeleton w-48 h-5"}/>
+                    <div className={"w-48 h-5"}/>
                     <div className={"flex flex-col gap-2"}>
-                        <div className={"skeleton w-full h-4"}/>
-                        <div className={"skeleton w-full h-4"}/>
-                        <div className={"skeleton w-1/2 h-4"}/>
+                        <Skeleton className={"w-full h-4"}/>
+                        <Skeleton className={"w-full h-4"}/>
+                        <Skeleton className={"w-1/2 h-4"}/>
                     </div>
-                    <div className={"skeleton w-1/2 h-5"}/>
-                    <div className={"skeleton w-full h-10"}/>
-                    <div className={"skeleton w-1/2 h-5"}/>
+                    <Skeleton className={"w-1/2 h-5"}/>
+                    <Skeleton className={"w-full h-10"}/>
+                    <Skeleton className={"w-1/2 h-5"}/>
                     <div className={"flex flex-col md:flex-row max-md:items-center gap-6"}>
-                        <div className={"skeleton w-1/2 h-5"}/>
-                        <div className={"skeleton w-1/2 h-5"}/>
+                        <Skeleton className={"w-1/2 h-5"}/>
+                        <Skeleton className={"w-1/2 h-5"}/>
                     </div>
-                    <div className={"skeleton w-full md:w-1/2 h-16 self-end"}/>
+                    <Skeleton className={"w-full md:w-1/2 h-16 self-end"}/>
                 </div>
             </div>
             <div className="divider my-7"/>
-            <div className={"skeleton w-full h-32"}/>
+            <Skeleton className={"w-full h-32"}/>
             <div className="divider my-7"/>
-            <div className={"skeleton w-full h-20"}/>
+            <Skeleton className={"w-full h-20"}/>
             <div className={"flex items-center gap-5 my-5"}>
-                <div className={"skeleton flex-1 h-80"}/>
-                <div className={"skeleton flex-1 h-80"}/>
-                <div className={"skeleton max-md:hidden flex-1 h-80"}/>
-                <div className={"skeleton max-lg:hidden flex-1 h-80"}/>
+                <Skeleton className={"flex-1 h-80"}/>
+                <Skeleton className={"flex-1 h-80"}/>
+                <Skeleton className={"max-md:hidden flex-1 h-80"}/>
+                <Skeleton className={"max-lg:hidden flex-1 h-80"}/>
             </div>
 
         </>
--- app/products/loading.tsx
+++ app/products/loading.tsx
@@ -1,23 +1,24 @@
+import {Skeleton} from "@/components/ui/skeleton";
+
 function Loading() {
 
     return (
         <div className={"flex flex-col"}>
             <div className={`flex justify-between items-center my-5 w-full`}>
-                <div className={"skeleton w-28 md:w-44 h-5"}/>
-                <div className={"skeleton w-28 md:w-44 h-5"}/>
+                <Skeleton className={"w-28 md:w-44 h-5"}/>
+                <Skeleton className={"w-28 md:w-44 h-5"}/>
             </div>
             <div
                 className={"grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 content-start"}>
                 {
                     Array.from({length: 10}).map((_, index) => {
                         return (
-                            <div key={index} className={"skeleton h-56 sm:h-96"}/>
+                            <Skeleton key={index} className={"h-56 sm:h-96"}/>
                         )
                     })
                 }
             </div>
-            <div className={"skeleton w-1/3 h-12 my-7 mx-auto"}/>
-
+            <Skeleton className={"w-1/3 h-12 my-7 mx-auto"}/>
         </div>
     )
 }
