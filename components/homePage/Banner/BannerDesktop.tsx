2b7d7a52ee212ba6374b49662054b8b0cbddb41d
Morta <mr.tajik1999@gmail.com>
2024-01-16 22:20:38 +0330

    ref(Banner): migrate to shadcn

--- components/homePage/Banner/BannerDesktop.tsx
+++ components/homePage/Banner/BannerDesktop.tsx
@@ -1,23 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
+import {Button} from "@/components/ui/button";
 
-function BannerDesktop () {
+function BannerDesktop() {
     return (
         <div className={"hidden md:grid grid-cols-6 mb-10 px-1"}>
             <div className={"flex flex-col justify-center gap-8 lg:gap-12 py-4 lg:py-8 bg-primary col-span-2 px-5"}>
-                <h1 className={"text-bas lg:text-xl text-white font-dana-black"}>
+                <h1 className={"text-base lg:text-xl text-white font-dana-black"}>
                     خانه رویایی خود را به واقعیت تبدیل کنید
                 </h1>
                 <p className={"text-justify text-white text-xs lg:text-sm xl:text-base lg:!leading-7"}>
                     از بین هزاران طرح کاغذ و پوستر دیواری فروشگاه دیوال برای فضای خانه و محل کار خود انتخاب و به
                     آسانی آنرا سفارشی کرده وآنلاین تحویل بگیرید
                 </p>
-                <Link className={"btn btn-sm lg:btn-md rounded-full w-max lg:w-56 !text-xs lg:!text-sm btn-ghost text-white border border-white hover:border-white"} href={"/products"}>
-                    مشاهده محصولات
-                </Link>
+                <Button variant={"outline"} className={"text-white"} asChild>
+                    <Link href={"/products"}>
+                        مشاهده محصولات
+
+                    </Link>
+                </Button>
             </div>
-            <Image src={"/pictures/banner-desktop.jpg"} width={750} height={500} className={"w-full h-full object-cover col-span-4"} priority alt={"banner-image"}/>
+            <Image src={"/pictures/banner-desktop.jpg"} width={750} height={500}
+                   className={"w-full h-full object-cover col-span-4"} priority alt={"banner-image"}/>
         </div>
     )
 }
+
 export default BannerDesktop
--- components/homePage/Banner/BannerMobile.tsx
+++ components/homePage/Banner/BannerMobile.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
 
-function BannerMobile ()  {
+function BannerMobile() {
     return (
-        <div className={"flex justify-center md:hidden mb-10 relative w-screen h-[80vh]"}>
+        <div className={"flex justify-center md:hidden mb-10 relative w-screen h-[80dvh]"}>
             <div className={"flex flex-col justify-center items-center px-2 gap-5 z-20 bg-black/60"}>
                 <h1 className={"text-white text-center font-dana-black text-2xl"}>
                     خانه رویایی خود را به واقعیت تبدیل کنید
@@ -12,9 +13,11 @@ function BannerMobile ()  {
                     از بین هزاران طرح کاغذ و پوستر دیواری فروشگاه دیوال برای فضای خانه و محل کار خود انتخاب و به
                     آسانی آنرا سفارشی کرده وآنلاین تحویل بگیرید
                 </p>
-                <Link className={"btn btn-primary rounded-full w-max"} href={"/products"}>
-                    مشاهده محصولات
-                </Link>
+                <Button asChild>
+                    <Link href={"/products"}>
+                        مشاهده محصولات
+                    </Link>
+                </Button>
             </div>
             <Image src={"/pictures/banner-mobile.jpg"} alt={"banner"} fill priority className={"object-cover"}/>
         </div>
