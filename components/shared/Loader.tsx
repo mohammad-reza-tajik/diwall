ec62c5392bd7cc3b58e5d05eb5e38059cdd0d0f2
Morta <mr.tajik1999@gmail.com>
2024-01-20 08:46:27 +0330

    style(Loader): reduce border size

--- components/shared/Loader.tsx
+++ components/shared/Loader.tsx
@@ -4,7 +4,7 @@ interface Props {
     className?:string;
 }
 function Loader({className}:Props) {
-    return (<span className={cn("size-7 border-4 border-primary border-x-transparent rounded-full inline-block animate-spin",className)}></span>)
+    return (<span className={cn("size-7 border-2 border-primary border-x-transparent rounded-full inline-block animate-spin",className)}></span>)
 }
 
 export default Loader;
