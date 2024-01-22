d2a007908e316d2ea0fa0d7e4f17c56765df6b00
Morta <mr.tajik1999@gmail.com>
2024-01-22 16:04:12 +0330

    ref(useAuth): set token cookie after login action
    token cookie used to be set in login reducer but now it happens in useAuth hook

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -3,15 +3,16 @@ import {userActions, useAppDispatch} from "@/store";
 import {login, signup} from "@/actions/user/auth";
 import {enqueueSnackbar} from "notistack";
 import {SubmitHandler, useForm} from "react-hook-form";
-import {loginSchema, TLoginSchema, signupSchema, TSignupSchema} from "@/types/authForms";
+import {loginSchema, LoginSchema, signupSchema, SignupSchema} from "@/types/user";
 import {zodResolver} from "@hookform/resolvers/zod";
+import Cookies from "js-cookie";
 
 const useAuth = (formType: "login" | "signup" = "login") => {
 
     const dispatch = useAppDispatch();
     const router = useRouter();
 
-    const form = useForm<TLoginSchema | TSignupSchema>({
+    const form = useForm<LoginSchema | SignupSchema>({
         resolver: zodResolver(formType === "login" ? loginSchema : signupSchema),
         defaultValues: {
             identifier: "",
@@ -21,14 +22,14 @@ const useAuth = (formType: "login" | "signup" = "login") => {
         }
     });
 
-    const submitHandler: SubmitHandler<TLoginSchema | TSignupSchema> = async (data) => {
+    const submitHandler: SubmitHandler<LoginSchema | SignupSchema> = async (data) => {
         try {
             let res: any;
 
             if (formType === "signup") {
                 res = await signup(signupSchema.parse(data));
             } else {
-                res = await login(loginSchema.parse(data))
+                res = await login(loginSchema.parse(data));
             }
 
             if (!res.ok) {
@@ -37,7 +38,8 @@ const useAuth = (formType: "login" | "signup" = "login") => {
 
             form.reset();
 
-            dispatch(userActions.login({user: res.user, token: res.token}));
+            dispatch(userActions.login(res.user));
+            Cookies.set("token",res.token);
             enqueueSnackbar(res.message, {
                 variant: "success",
             });
