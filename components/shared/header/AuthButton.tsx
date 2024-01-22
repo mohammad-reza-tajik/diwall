49360808d4961b704ee14af6cd822cb4c39ff2ab
Morta <mr.tajik1999@gmail.com>
2024-01-22 17:34:38 +0330

    ref(AuthButton): migrate to shadcn

--- components/shared/header/AuthButton.tsx
+++ components/shared/header/AuthButton.tsx
@@ -1,27 +1,23 @@
-"use client"
 import Link from "next/link";
 import {Login} from "@/components/shared/Icons";
-import { useState} from "react";
-import {UserType} from "@/db/userModel";
 import UserMenu from "@/components/shared/header/UserMenu";
+import {type User} from "@/types/user";
+import {Button} from "@/components/ui/button";
 
-function AuthButton(user: UserType) {
-    const [openMenu, setOpenMenu] = useState(false)
+interface Props {
+    user ?: User
+}
+function AuthButton({user}: Props) {
 
     return (
-        user?.username === null ?
-            <Link className={"btn btn-primary w-48 rounded-full"} href={"/auth"}>
-                <Login className={"fill-white size-6"}/>
-                ورود / ثبت نام
-            </Link> :
-            <div role={"button"} className={"flex justify-center items-center w-48 gap-2 bg-primary text-white fill-white shadow relative py-2 rounded-full"} aria-label={"باز کردن منوی کاربر"}
-                    onClick={() => setOpenMenu((prevState)=>!prevState)}
-            >
-                <span className={"line-clamp-1"}>{user?.username}</span>
-                <UserMenu user={user} open={openMenu}/>
-            </div>
-
-
+        !user ?
+            <Button asChild>
+                <Link className={"w-48 gap-2"} href={"/auth"}>
+                    <Login className={"fill-white size-6"}/>
+                    ورود / ثبت نام
+                </Link>
+            </Button> :
+            <UserMenu user={user}/>
     )
 }
 
