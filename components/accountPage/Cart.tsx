fbbb71f25ead18536ded673c635642449067fd9a
Morta <mr.tajik1999@gmail.com>
2024-01-26 22:18:39 +0330

    💥feat(Cart): add calculating the cart price functionality💥

--- components/accountPage/Cart.tsx
+++ components/accountPage/Cart.tsx
@@ -1,6 +1,7 @@
 import CartItem from "./CartItem";
 import type {User} from "@/types/user";
 import {cn} from "@/lib/utils";
+import {Circle} from "@/components/shared/Icons";
 
 interface Props {
     user?: User
@@ -8,7 +9,7 @@ interface Props {
 
 function Cart({user}: Props) {
     return (
-        <ul className={cn("grid gap-3 min-h-full grid-cols-3 content-start", {
+        <div className={cn("grid gap-3 min-h-full grid-cols-3 content-start", {
             "content-center": user?.cart.length === 0,
         })}>
             {
@@ -18,7 +19,20 @@ function Cart({user}: Props) {
                     </p> :
                     user.cart.map((item) => <CartItem product={item.product} key={item.product._id}/>)
             }
-        </ul>
+            <div className={"flex items-center col-span-3 justify-center md:justify-end gap-2 py-7 text-xs md:text-sm"}>
+                <Circle className={"fill-primary size-4"} />
+                <span className={"font-dana-bold"}>بهای قابل پرداخت :</span>
+                <span>
+                {
+                    user?.cart.reduce((finalPrice , item )=>{
+                        return item.quantity * item.product.price + finalPrice
+                    },0)
+                }
+                &nbsp;
+                تومان
+                </span>
+            </div>
+        </div>
     )
 }
 
