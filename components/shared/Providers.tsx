f14c351bb6742392f714cee1f1a330b2a2c9c2f2
Morta <mr.tajik1999@gmail.com>
2024-02-03 22:00:15 +0330

    feat: add next-themes package and its ThemeProvider
    set the styles of ToastBar using css variables to enable dark mode
    fixed the service worker path

--- components/shared/Providers.tsx
+++ components/shared/Providers.tsx
@@ -6,6 +6,7 @@ import {Close} from "@/components/shared/Icons";
 import toast, {Toaster, ToastBar} from 'react-hot-toast';
 import {Button} from "@/components/ui/button";
 import {useEffect} from "react";
+import {ThemeProvider} from "next-themes";
 
 
 interface Props {
@@ -14,36 +15,42 @@ interface Props {
 
 function Providers({children}: Props) {
 
-    useEffect(()=>{
-        (async ()=>{
-            if (typeof navigator.serviceWorker !== 'undefined') {
-                await navigator.serviceWorker.register('sw.js');
+    useEffect(() => {
+        (async () => {
+            if (typeof navigator.serviceWorker !== "undefined") {
+                await navigator.serviceWorker.register("/sw.js");
             }
         })();
-    },[])
+    }, [])
 
 
     return (
         <StoreProvider store={store}>
             <DirectionProvider dir={"rtl"}>
-                {children}
-                <Toaster toastOptions={{duration: 5000,position:"bottom-center",style:{fontSize : 14}}}>
-                    {(t) => (
-                        <ToastBar toast={t}>
-                            {({icon, message}) => (
-                                <>
-                                    {icon}
-                                    {message}
-                                    {t.type !== "loading" && (
-                                        <Button size={"icon"} variant={"ghost"} onClick={() => toast.dismiss(t.id)}>
-                                            <Close className={"size-5"}/>
-                                        </Button>
-                                    )}
-                                </>
-                            )}
-                        </ToastBar>
-                    )}
-                </Toaster>
+                <ThemeProvider attribute={"class"} defaultTheme={"dark"} enableSystem disableTransitionOnChange>
+                    {children}
+                    <Toaster toastOptions={{duration: 5000, position: "bottom-center", style: {fontSize: 14}}}>
+                        {(t) => (
+                            <ToastBar toast={t} style={{
+                                backgroundColor: "hsl(var(--background))",
+                                color: "hsl(var(--foreground))",
+                                fill: "hsl(var(--foreground))",
+                            }}>
+                                {({icon, message}) => (
+                                    <>
+                                        {icon}
+                                        {message}
+                                        {t.type !== "loading" && (
+                                            <Button size={"icon"} variant={"ghost"} onClick={() => toast.dismiss(t.id)}>
+                                                <Close className={"size-5"}/>
+                                            </Button>
+                                        )}
+                                    </>
+                                )}
+                            </ToastBar>
+                        )}
+                    </Toaster>
+                </ThemeProvider>
             </DirectionProvider>
         </StoreProvider>
     )
