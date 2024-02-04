44790457f6f03a5affb25cdd6eae6764e5a7ec2e
Morta <mr.tajik1999@gmail.com>
2024-02-04 12:51:16 +0330

    ref(ThemeSwitcher): enhance accessibility

--- components/shared/ThemeSwitcher.tsx
+++ components/shared/ThemeSwitcher.tsx
@@ -6,14 +6,21 @@ import {Moon, Sun} from "@/components/shared/Icons";
 
 function ThemeSwitcher(props: ButtonHTMLAttributes<HTMLButtonElement>) {
 
-    const {setTheme} = useTheme();
+    const {theme,setTheme} = useTheme();
+
+    const switchThemeHandler = () => {
+        if (theme === "dark") {
+            setTheme("light");
+        } else {
+            setTheme("dark");
+        }
+    }
 
     return (
-        <Button size={"icon"} variant={"outline"} {...props}>
-            <Sun onClick={() => setTheme("light")} className={"hidden dark:inline-block"}/>
-            <Moon onClick={() => setTheme("dark")} className={"inline-block dark:hidden"}/>
+        <Button size={"icon"} variant={"outline"} {...props} onClick={switchThemeHandler} aria-label={"تغییر تم سایت"}>
+            <Sun className={"inline-block dark:hidden"}/>
+            <Moon className={"hidden dark:inline-block"}/>
         </Button>
-
     )
 }
 
