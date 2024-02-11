d396ba35a0c57dd8211f0772c1399ad0ffb57f7d
Morta <mr.tajik1999@gmail.com>
2024-02-11 09:57:28 +0330

    feat: add ThemeSwitcher to the MainNav

--- components/shared/MainNav.tsx
+++ components/shared/MainNav.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import navLinks from "@/constants/navLinks";
-import {Phone} from "@/components/shared/Icons";
+import ThemeSwitcher from "@/components/shared/ThemeSwitcher";
 
 function MainNav() {
 
@@ -8,24 +8,17 @@ function MainNav() {
         <div className={"hidden md:flex justify-between my-5"}>
             <nav className={"flex items-center gap-6 text-sm lg:text-base"}>
                 {
-                    navLinks.map((link , index) => (
-                        <Link key={index} href={link.href} className={"flex items-center gap-1 hover:text-primary hover:fill-primary transition-colors"}>
-                            {link.icon}
-                            {link.text}
-                        </Link>
+                    navLinks.map((link, index) => (
+                            <Link key={index} href={link.href}
+                                  className={"flex items-center gap-1 text-xs lg:text-base hover:text-primary hover:fill-primary transition-colors"}>
+                                {link.icon}
+                                {link.text}
+                            </Link>
                         )
                     )
                 }
             </nav>
-            <div className={"hidden lg:flex"}>
-                <div className={"flex flex-col"}>
-                    <span className={"text-base font-dana-bold"}>021464879</span>
-                    <span className={"text-sm text-center"}>پشتیبانی</span>
-                </div>
-                <div className={"flex justify-center items-center"}>
-                    <Phone className={"fill-primary size-8"} />
-                </div>
-            </div>
+            <ThemeSwitcher/>
         </div>
     )
 }
