125a1330e0c4960a10cc1f87f1fde1c5fa60caca
Morta <mr.tajik1999@gmail.com>
2024-04-07 16:27:48 +0330

    chore(deps): update all dependencies and migrate to pnpm

--- components/homePage/Banner/BannerDesktop.tsx
+++ components/homePage/Banner/BannerDesktop.tsx
@@ -5,18 +5,17 @@ import {Button} from "@/components/ui/button";
 function BannerDesktop() {
     return (
         <div className={"hidden md:grid grid-cols-6 mb-10 px-1"}>
-            <div className={"flex flex-col justify-center gap-8 lg:gap-12 py-4 lg:py-8 bg-primary col-span-2 px-5"}>
-                <h1 className={"text-base lg:text-xl text-white font-dana-black"}>
+            <div className={"flex flex-col justify-center gap-8 lg:gap-12 py-4 lg:py-8 bg-primary text-primary-foreground col-span-2 px-5"}>
+                <h1 className={"text-base lg:text-xl font-dana-black"}>
                     خانه رویایی خود را به واقعیت تبدیل کنید
                 </h1>
-                <p className={"text-justify text-white text-xs lg:text-sm xl:text-base lg:!leading-7"}>
+                <p className={"text-justify text-xs lg:text-sm xl:text-base lg:!leading-7"}>
                     از بین هزاران طرح کاغذ و پوستر دیواری فروشگاه دیوال برای فضای خانه و محل کار خود انتخاب و به
                     آسانی آنرا سفارشی کرده وآنلاین تحویل بگیرید
                 </p>
-                <Button variant={"outline"} className={"text-white"} asChild>
+                <Button variant={"outline"} asChild>
                     <Link href={"/products"}>
                         مشاهده محصولات
-
                     </Link>
                 </Button>
             </div>
--- components/homePage/Banner/BannerMobile.tsx
+++ components/homePage/Banner/BannerMobile.tsx
@@ -5,11 +5,11 @@ import {Button} from "@/components/ui/button";
 function BannerMobile() {
     return (
         <div className={"flex justify-center md:hidden mb-10 relative w-screen h-[80dvh]"}>
-            <div className={"flex flex-col justify-center items-center px-2 gap-5 z-20 bg-black/60"}>
-                <h1 className={"text-white text-center font-dana-black text-2xl"}>
+            <div className={"flex flex-col justify-center items-center px-2 gap-5 z-20 bg-black/60 text-primary-foreground"}>
+                <h1 className={"text-center font-dana-black text-2xl"}>
                     خانه رویایی خود را به واقعیت تبدیل کنید
                 </h1>
-                <p className={"text-white text-center text-base sm:text-lg leading-10"}>
+                <p className={"text-center text-base sm:text-lg leading-10"}>
                     از بین هزاران طرح کاغذ و پوستر دیواری فروشگاه دیوال برای فضای خانه و محل کار خود انتخاب و به
                     آسانی آنرا سفارشی کرده وآنلاین تحویل بگیرید
                 </p>
