219b602553888c94912f5799f44591ae7a74107e
Morta <mr.tajik1999@gmail.com>
2024-11-05 22:41:21 +0330

    feat(utils): add debounce function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function debounce(func: (...args: any[]) => void, wait: number = 500): (...args: any[]) => void {
+  let timeout: NodeJS.Timeout;
+  return function(...args: any[]) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  };
+}
