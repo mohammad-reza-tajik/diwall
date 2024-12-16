7633d2d350adda97cb1ab90470fb94981adc71a4
Morta <mr.tajik1999@gmail.com>
2024-12-16 13:24:09 +0330

    chore: disable eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from "next";
 const nextConfig = {
     reactStrictMode: true,
     poweredByHeader: false,
+    eslint : {
+        ignoreDuringBuilds : true
+    }
 } satisfies NextConfig;
 
 module.exports = nextConfig
