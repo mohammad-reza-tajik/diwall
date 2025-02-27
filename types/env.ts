6d3bab9c4ab5d58d45ab0ad1eadb4ea57f5c5932
Morta <mr.tajik1999@gmail.com>
2025-02-27 23:35:21 +0330

    fix: type of the env variables

--- types/env.ts
+++ types/env.ts
@@ -1,14 +1,18 @@
 import {z} from "zod";
 
+const regExp = /^\d+(\s?(Years?|Yrs?|Y|Weeks?|W|Days?|D|Hours?|Hrs?|H|Minutes?|Mins?|M|Seconds?|Secs?|s|Milliseconds?|Msecs?|Ms))?$/i;
+
+
 const envSchema = z.object({
     MONGODB_URL : z.string(),
     JWT_SECRET : z.string(),
-    JWT_EXPIRES_IN : z.string(),
+    JWT_EXPIRES_IN : z.string().regex(regExp),
 
 })
 
 
 envSchema.parse(process.env);
+console.log(process.env.JWT_EXPIRES_IN.match(regExp))
 
 declare global {
     namespace NodeJS{
--- utils/generateToken.ts
+++ utils/generateToken.ts
@@ -1,6 +1,7 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 function generateToken (id : string) {
+    // @ts-ignore
     return jwt.sign({_id: id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN})
 }
 
